be10dc36078c5d84f7d1cb71d970270362f4a7f8
Andy Pavlo <pavlo@cs.brown.edu>
2011-09-27 03:31:57 +0000

    Added initial port of TableDataIterable from H-Store
    git-svn-id: https://oltpbenchmark.googlecode.com/svn/trunk@12 8c9255d6-0b70-036a-fcb6-8a999947b34c

---
+++ src/com/oltpbenchmark/util/TableDataIterable.java
@@ -0,0 +1,164 @@
+/*******************************************************************************
+ * oltpbenchmark.com
+ *  
+ *  Project Info:  http://oltpbenchmark.com
+ *  Project Members:    Carlo Curino <carlo.curino@gmail.com>
+ *              Evan Jones <ej@evanjones.ca>
+ *              DIFALLAH Djellel Eddine <djelleleddine.difallah@unifr.ch>
+ *              Andy Pavlo <pavlo@cs.brown.edu>
+ *              CUDRE-MAUROUX Philippe <philippe.cudre-mauroux@unifr.ch>  
+ *                  Yang Zhang <yaaang@gmail.com> 
+ * 
+ *  This library is free software; you can redistribute it and/or modify it under the terms
+ *  of the GNU General Public License as published by the Free Software Foundation;
+ *  either version 3.0 of the License, or (at your option) any later version.
+ * 
+ *  This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU Lesser General Public License for more details.
+ ******************************************************************************/
+package com.oltpbenchmark.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Iterator;
+
+import au.com.bytecode.opencsv.CSVReader;
+
+/**
+ * @author pavlo
+ */
+public class TableDataIterable implements Iterable<Object[]> {
+    private final File table_file;
+    private final CSVReader reader;
+    private final int types[]; // TODO
+    private final boolean fkeys[]; // TODO
+    private final boolean nullable[]; // TODO
+    private final boolean auto_generate_first_column;
+    
+    private final DateFormat timestamp_formats[] = new DateFormat[] {
+        new SimpleDateFormat("yyyy-MM-dd"),
+        new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"),
+        new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS"),
+    };
+    private int line_ctr = 0;
+    
+    /**
+     * Constructor
+     * @param table_file
+     * @param has_header whether we expect the data file to include a header in the first row
+     * @param auto_generate_first_column TODO
+     * @throws Exception
+     */
+    public TableDataIterable(File table_file, int types[], boolean fkeys[], boolean nullable[], boolean has_header, boolean auto_generate_first_column) throws Exception {
+        assert(types.length == fkeys.length);
+        assert(types.length == nullable.length);
+        this.table_file = table_file;
+        this.types = types;
+        this.fkeys = fkeys;
+        this.nullable = nullable;
+        this.auto_generate_first_column = auto_generate_first_column;
+        
+        BufferedReader in = new BufferedReader(new FileReader(this.table_file));
+        this.reader = new CSVReader(in);
+        
+        // Throw away the first row if there is a header
+        if (has_header) {
+            this.reader.readNext();
+            this.line_ctr++;
+        }
+    }
+    
+    
+    public Iterator<Object[]> iterator() {
+        return (new TableIterator());
+    }
+    
+    public class TableIterator implements Iterator<Object[]> {
+        String[] next = null;
+
+        private void getNext() {
+            if (next == null) {
+                try {
+                    next = reader.readNext();
+                } catch (Exception ex) {
+                    throw new RuntimeException("Unable to retrieve tuples from '" + table_file + "'", ex);
+                }
+            }
+        }
+        
+        @Override
+        public boolean hasNext() {
+            this.getNext();
+            return (next != null);
+        }
+        
+        @Override
+        public Object[] next() {
+            this.getNext();
+            if (next == null) return (next);
+            String row[] = null;
+            synchronized (this) {
+                row = this.next;
+                this.next = null;
+            } // SYNCH
+            
+            Object tuple[] = new Object[types.length];
+            int row_idx = 0;
+            for (int col_idx = 0; col_idx < types.length; col_idx++) {
+                // Auto-generate first column
+                if (col_idx == 0 && auto_generate_first_column) {
+                    tuple[col_idx] = new Long(line_ctr) ;
+                }
+                // Null Values
+                else if (row_idx >= row.length) {
+                    tuple[col_idx] = null;
+                }
+                // Foreign Keys
+                else if (fkeys[col_idx]) {
+                    tuple[col_idx] = row[row_idx++];
+                }
+                // Timestamps
+                else if (types[col_idx] == 100) { // XXX VoltType.TIMESTAMP) {
+                    for (DateFormat f : timestamp_formats) {
+                        try {
+                            tuple[col_idx] = f.parse(row[row_idx]);
+                        } catch (ParseException ex) {
+                            // Ignore...
+                        }
+                        if (tuple[col_idx] != null) break;
+                    } // FOR
+                    if (tuple[col_idx] == null) {
+                        throw new RuntimeException(String.format("Line %d: Invalid timestamp format '%s' for column #%d",
+                                                                 TableDataIterable.this.line_ctr, row[row_idx], col_idx));
+                    }
+                    row_idx++;
+                }
+                // Store string (truncate if necessary)
+                else if (types[col_idx] == 200) { // XXX VoltType.STRING) {
+                    tuple[col_idx] = row[row_idx++];
+                }    
+                // Default: Cast the string into the proper type
+                else {
+                    if (row[row_idx].isEmpty() && nullable[col_idx]) {
+                        tuple[col_idx] = null;
+                    } else {
+                        // TODO
+                    }
+                    row_idx++;
+                }
+            } // FOR
+            TableDataIterable.this.line_ctr++;
+            return (tuple);
+        }
+        
+        @Override
+        public void remove() {
+            throw new RuntimeException("Unimplemented operation");
+        }
+    }
+}
