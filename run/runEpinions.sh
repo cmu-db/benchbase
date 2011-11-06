8a1bceb295c899a2a3442f71600d228364b34a59
Carlo Curino <carlo.curino@gmail.com>
2011-11-06 08:36:24 +0000

    small script and config file changes
    git-svn-id: https://oltpbenchmark.googlecode.com/svn/trunk@48 8c9255d6-0b70-036a-fcb6-8a999947b34c

---
+++ run/runEpinions.sh
@@ -0,0 +1,1 @@
+java -Xmx1024m -cp `run/classpath.sh` com.oltpbenchmark.DBWorkload -b epinions -c config/sample_epinions_config.xml
--- run/runWiki.sh
+++ run/runWiki.sh
@@ -1,3 +1,2 @@
-# run wikipedia with 20 terminals over a small trace with 10 sec warmup and 50 sec measurement time
-java -Xmx1024m -cp `run/classpath.sh` com.oltpbenchmark.DBWorkload -b wikipedia -c config/sample_wiki_config.xml
+java -Xmx1024m -cp `run/classpath.sh` com.oltpbenchmark.DBWorkload -b wikipedia -c config/sample_wiki_config.xml -o ~/trans-wiki1k-io 2>&1 > ~/err-wiki1k-io
 
