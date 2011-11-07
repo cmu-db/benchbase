b9ded96bb3c3c991105391c4a6e1397d9d3ba5c9
Carlo Curino <carlo.curino@gmail.com>
2011-11-07 05:10:45 +0000

    twitter ddl
    git-svn-id: https://oltpbenchmark.googlecode.com/svn/trunk@63 8c9255d6-0b70-036a-fcb6-8a999947b34c

--- run/runWiki.sh
+++ run/runWiki.sh
@@ -1,2 +1,2 @@
-java -Xmx1024m -cp `run/classpath.sh` com.oltpbenchmark.DBWorkload -b wikipedia -c config/sample_wiki_config.xml -o ~/trans-wiki1k-io 2>&1 > ~/err-wiki1k-io
+java -Xmx1024m -cp `run/classpath.sh` com.oltpbenchmark.DBWorkload -b wikipedia -c config/sample_wiki_config.xml 
 
