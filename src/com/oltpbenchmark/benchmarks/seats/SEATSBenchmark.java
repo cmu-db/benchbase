7f14bf51f0be4be351363178e3a0c2d6982bd6a8
Andy Pavlo <pavlo@cs.brown.edu>
2011-12-12 00:22:11 +0000

    Checking in some initial files for SEATS. I'm still not ready to port everything over just yet... Will try to finish up my fixes tomorrow
    git-svn-id: https://oltpbenchmark.googlecode.com/svn/trunk@144 8c9255d6-0b70-036a-fcb6-8a999947b34c

---
+++ src/com/oltpbenchmark/benchmarks/seats/SEATSBenchmark.java
@@ -0,0 +1,68 @@
+/***************************************************************************
+ *  Copyright (C) 2011 by H-Store Project                                  *
+ *  Brown University                                                       *
+ *  Massachusetts Institute of Technology                                  *
+ *  Yale University                                                        *
+ *                                                                         *
+ *  http://hstore.cs.brown.edu/                                            *
+ *                                                                         *
+ *  Permission is hereby granted, free of charge, to any person obtaining  *
+ *  a copy of this software and associated documentation files (the        *
+ *  "Software"), to deal in the Software without restriction, including    *
+ *  without limitation the rights to use, copy, modify, merge, publish,    *
+ *  distribute, sublicense, and/or sell copies of the Software, and to     *
+ *  permit persons to whom the Software is furnished to do so, subject to  *
+ *  the following conditions:                                              *
+ *                                                                         *
+ *  The above copyright notice and this permission notice shall be         *
+ *  included in all copies or substantial portions of the Software.        *
+ *                                                                         *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        *
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     *
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. *
+ *  IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR      *
+ *  OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,  *
+ *  ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR  *
+ *  OTHER DEALINGS IN THE SOFTWARE.                                        *
+ ***************************************************************************/
+package com.oltpbenchmark.benchmarks.seats;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.oltpbenchmark.WorkloadConfiguration;
+import com.oltpbenchmark.api.BenchmarkModule;
+import com.oltpbenchmark.api.Worker;
+import com.oltpbenchmark.benchmarks.tatp.procedures.DeleteCallForwarding;
+import com.oltpbenchmark.catalog.Table;
+
+public class SEATSBenchmark extends BenchmarkModule {
+
+	public SEATSBenchmark(WorkloadConfiguration workConf) {
+		super("seats", workConf);
+	}
+	
+	@Override
+	protected Package getProcedurePackageImpl() {
+		return (DeleteCallForwarding.class.getPackage());
+	}
+
+	@Override
+	protected List<Worker> makeWorkersImpl(boolean verbose) throws IOException {
+		List<Worker> workers = new ArrayList<Worker>();
+//		for (int i = 0; i < workConf.getTerminals(); ++i) {
+//			workers.add(new SEATSWorker(i, this));
+//		} // FOR
+		return (workers);
+	}
+	
+	@Override
+	protected void loadDatabaseImpl(Connection conn, Map<String, Table> tables) throws SQLException {
+//		SEATSLoader loader = new SEATSLoader(conn, this.workConf, tables);
+//		loader.load(); // Blocking...
+	}
+}
