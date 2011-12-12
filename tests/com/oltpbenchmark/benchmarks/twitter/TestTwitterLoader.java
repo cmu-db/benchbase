9d6e1a3e549a5994351c28502a6e06423c931936
DIFALLAH Djellel Eddine <difallah@gmail.com>
2011-12-12 12:32:29 +0000

    Fix TwitterLoader test: The minimum scale factor should be 0.1 otherwise no Followers are inserted.
    git-svn-id: https://oltpbenchmark.googlecode.com/svn/trunk@161 8c9255d6-0b70-036a-fcb6-8a999947b34c

--- tests/com/oltpbenchmark/benchmarks/twitter/TestTwitterLoader.java
+++ tests/com/oltpbenchmark/benchmarks/twitter/TestTwitterLoader.java
@@ -11,6 +11,7 @@ public class TestTwitterLoader extends AbstractTestLoader<TwitterBenchmark> {
     @Override
     protected void setUp() throws Exception {
         super.setUp(TwitterBenchmark.class, IGNORED_TABLES, TestTwitterBenchmark.PROC_CLASSES);
+        this.workConf.setScaleFactor(0.1);
     }
 
 }
