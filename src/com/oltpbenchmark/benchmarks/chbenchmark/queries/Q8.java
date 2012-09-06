a9536244f83ab46d762a6b3a846b7126b97ef0a6
Dmytro Vorona <alendit@gmail.com>
2012-09-06 18:46:32 +0200

    Queries 6-9 added

--- src/com/oltpbenchmark/benchmarks/chbenchmark/queries/Q8.java
+++ src/com/oltpbenchmark/benchmarks/chbenchmark/queries/Q8.java
@@ -7,9 +7,9 @@ public class Q8 extends GenericQuery {
 	@Override
 	protected SQLStmt getStmtSQL() {
 		return new SQLStmt(
-"select 	  extract ( year   from  o_entry_d)  as  l_year,"
- + "	  sum ( case   when  n2.n_name =  'Germany'   then  ol_amount  else   0   end ) /  sum (ol_amount)  as  mkt_share"
- + " from 	 item, supplier, stock, orderline, orders, customer, nation n1, nation n2, region"
+"select 	  extract(year   from  o_entry_d)  as  l_year,"
+ + "	  sum(case   when  n2.n_name =  'Germany'   then  ol_amount  else 0 end) /  sum(ol_amount)  as  mkt_share"
+ + " from 	 item, supplier, stock, order_line, oorder, customer, nation n1, nation n2, region"
  + " where 	 i_id = s_i_id"
  + "	  and  ol_i_id = s_i_id"
  + "	  and  ol_supply_w_id = s_w_id"
@@ -28,10 +28,8 @@ public class Q8 extends GenericQuery {
  + "	  and  o_entry_d  between   '2007-01-02 00:00:00.000000'   and   '2012-01-02 00:00:00.000000' "
  + "	  and  i_data  like   '%b' "
  + "	  and  i_id = ol_i_id"
- + " group   by   extract ( year   from  o_entry_d)"
+ + " group   by   extract(year   from  o_entry_d)"
  + " order   by  l_year"
- + ""
- + ""
                                   );
 	}
 }
--- src/com/oltpbenchmark/benchmarks/chbenchmark/queries/Q9.java
+++ src/com/oltpbenchmark/benchmarks/chbenchmark/queries/Q9.java
@@ -7,8 +7,8 @@ public class Q9 extends GenericQuery {
 	@Override
 	protected SQLStmt getStmtSQL() {
 		return new SQLStmt(
-"select 	 n_name,  extract ( year   from  o_entry_d)  as  l_year,  sum (ol_amount)  as  sum_profit"
- + " from 	 item, stock, supplier, orderline, orders, nation"
+"select 	 n_name,  extract(year   from  o_entry_d)  as  l_year,  sum(ol_amount) as sum_profit"
+ + " from 	 item, stock, supplier, order_line, oorder, nation"
  + " where 	 ol_i_id = s_i_id"
  + "	  and  ol_supply_w_id = s_w_id"
  + "	  and   mod ((s_w_id * s_i_id),  10000 ) = su_suppkey"
@@ -18,10 +18,8 @@ public class Q9 extends GenericQuery {
  + "	  and  ol_i_id = i_id"
  + "	  and  su_nationkey = n_nationkey"
  + "	  and  i_data  like   '%BB' "
- + " group   by  n_name,  extract ( year   from  o_entry_d)"
- + " order   by  n_name, l_year  desc "
- + ""
- + ""
+ + " group   by  n_name,  extract(year   from  o_entry_d)"
+ + " order   by  n_name, l_year  desc"
                                   );
 	}
 }
