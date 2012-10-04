e760354d9b8a75c961739b8a5eaa41e4ec717c86
Dmytro Vorona <alendit@gmail.com>
2012-10-04 11:46:45 +0200

    Batch script completed

--- tools/run_mix_vs_single.py
+++ tools/run_mix_vs_single.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from subprocess import check_call, STDOUT
-import os
+from subprocess import check_call, STDOUT, Popen
+import os, sys
 
 PATH_TO_OLTP = ".."
+PATH_TO_PLOTTER = os.path.abspath("plot/plot_latencies.py")
 
 if __name__ == "__main__":
-    configs = [#('tpcc', 'config/tpcc_config_postres.xml'),
-                #('chbenchmark', 'config/chbench_config_postgres.xml'),
+    configs = [('tpcc', 'config/tpcc_config_postgre.xml'),
+                ('chbenchmark', 'config/hc_config_postgres.xml'),
                 ('tpcc,chbenchmark', 'config/mix_config_postgres.xml'),
                 ]
 
+    old_dir = os.getcwd()
+    os.chdir(PATH_TO_OLTP)
+    
     for config in configs:
-        old_dir = os.getcwd()
-        os.chdir(PATH_TO_OLTP)
-        print config
         call_args = ["./oltpbenchmark",
         '-b', config[0],
         '-c', config[1],
@@ -24,6 +25,14 @@ if __name__ == "__main__":
         '-s', "5",
         '-o', 'output',
         '--histograms']
-        print " ".join(call_args)
-        check_call(call_args)
+        print check_call(call_args)
+        
+        Popen([PATH_TO_PLOTTER,
+                    "output.raw",
+                    "{0}.png".format(config[1].split("/")[-1].split(".")[0]),
+                    ])
+                    
+
+    raw_input("Press enter to EXIT")
+    sys.exit(0)
 
