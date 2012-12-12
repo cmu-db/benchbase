40426c4dd0fa74a0e088652e5ce4e1807d808d5e
Dmytro Vorona <alendit@gmail.com>
2012-12-12 10:34:43 +0100

    CH queries fixed

--- src/com/oltpbenchmark/benchmarks/chbenchmark/queries/query14.sql
+++ src/com/oltpbenchmark/benchmarks/chbenchmark/queries/query14.sql
@@ -1,4 +1,4 @@
-SELECT (100.00 * sum(CASE WHEN i_data LIKE 'PR%' THEN ol_amount ELSE 0 END) / 1 + sum(ol_amount)) AS promo_revenue
+SELECT (100.00 * sum(CASE WHEN i_data LIKE 'PR%' THEN ol_amount ELSE 0 END) / (1 + sum(ol_amount))) AS promo_revenue
 FROM order_line,
      item
 WHERE ol_i_id = i_id
