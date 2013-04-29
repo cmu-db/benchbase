52fa8bb276b585b3e2328e50c6745bb3b2cf6fa8
Dmytro Vorona <alendit@gmail.com>
2013-04-29 19:02:45 +0200

    Added cascade delete to the primary key in chbenchmark tables

--- src/com/oltpbenchmark/benchmarks/chbenchmark/chbenchmark-mysql-ddl.sql
+++ src/com/oltpbenchmark/benchmarks/chbenchmark/chbenchmark-mysql-ddl.sql
@@ -12,7 +12,7 @@ create table region (
 create table nation (
    n_nationkey int not null,
    n_name char(25) not null,
-   n_regionkey int not null references region(r_regionkey),
+   n_regionkey int not null references region(r_regionkey) ON DELETE CASCADE,
    n_comment char(152) not null,
    PRIMARY KEY ( n_nationkey )
 );
@@ -21,7 +21,7 @@ create table supplier (
    su_suppkey int not null,
    su_name char(25) not null,
    su_address varchar(40) not null,
-   su_nationkey int not null references nation(n_nationkey),
+   su_nationkey int not null references nation(n_nationkey) ON DELETE CASCADE,
    su_phone char(15) not null,
    su_acctbal numeric(12,2) not null,
    su_comment char(101) not null,
