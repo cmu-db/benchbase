c9e76eab7f923c751f5dc219dcb4f261d43bc9dc
Tommy Reilly <treilly@nuodb.com>
2013-09-16 15:41:33 -0400

    Clean up ddl, use STRING instead of varchar and SMALLINT instead of int

--- src/com/oltpbenchmark/benchmarks/tatp/tatp-nuodb-ddl.sql
+++ src/com/oltpbenchmark/benchmarks/tatp/tatp-nuodb-ddl.sql
@@ -1,27 +1,27 @@
-DROP TABLE IF EXISTS SUBSCRIBER;
+DROP TABLE IF EXISTS SUBSCRIBER CASCADE;
 CREATE TABLE SUBSCRIBER (
    s_id INTEGER NOT NULL PRIMARY KEY,
-   sub_nbr VARCHAR(15) NOT NULL UNIQUE,
-   bit_1 int,
-   bit_2 int,
-   bit_3 int,
-   bit_4 int,
-   bit_5 int,
-   bit_6 int,
-   bit_7 int,
-   bit_8 int,
-   bit_9 int,
-   bit_10 int,
-   hex_1 int,
-   hex_2 int,
-   hex_3 int,
-   hex_4 int,
-   hex_5 int,
-   hex_6 int,
-   hex_7 int,
-   hex_8 int,
-   hex_9 int,
-   hex_10 int,
+   sub_nbr STRING NOT NULL UNIQUE,
+   bit_1 SMALLINT,
+   bit_2 SMALLINT,
+   bit_3 SMALLINT,
+   bit_4 SMALLINT,
+   bit_5 SMALLINT,
+   bit_6 SMALLINT,
+   bit_7 SMALLINT,
+   bit_8 SMALLINT,
+   bit_9 SMALLINT,
+   bit_10 SMALLINT,
+   hex_1 SMALLINT,
+   hex_2 SMALLINT,
+   hex_3 SMALLINT,
+   hex_4 SMALLINT,
+   hex_5 SMALLINT,
+   hex_6 SMALLINT,
+   hex_7 SMALLINT,
+   hex_8 SMALLINT,
+   hex_9 SMALLINT,
+   hex_10 SMALLINT,
    byte2_1 SMALLINT,
    byte2_2 SMALLINT,
    byte2_3 SMALLINT,
@@ -39,11 +39,11 @@ CREATE TABLE SUBSCRIBER (
 DROP TABLE IF EXISTS ACCESS_INFO;
 CREATE TABLE ACCESS_INFO (
    s_id INTEGER NOT NULL,
-   ai_type int NOT NULL,
+   ai_type SMALLINT NOT NULL,
    data1 SMALLINT,
    data2 SMALLINT,
-   data3 VARCHAR(3),
-   data4 VARCHAR(5),
+   data3 STRING,
+   data4 STRING,
    PRIMARY KEY(s_id, ai_type),
    FOREIGN KEY (s_id) REFERENCES SUBSCRIBER (s_id)
 );
@@ -51,11 +51,11 @@ CREATE TABLE ACCESS_INFO (
 DROP TABLE IF EXISTS SPECIAL_FACILITY;
 CREATE TABLE SPECIAL_FACILITY (
    s_id INTEGER NOT NULL,
-   sf_type int NOT NULL,
-   is_active int NOT NULL,
+   sf_type SMALLINT NOT NULL,
+   is_active SMALLINT NOT NULL,
    error_cntrl SMALLINT,
    data_a SMALLINT,
-   data_b VARCHAR(5),
+   data_b STRING,
    PRIMARY KEY (s_id, sf_type),
    FOREIGN KEY (s_id) REFERENCES SUBSCRIBER (s_id)
 );
@@ -63,10 +63,10 @@ CREATE TABLE SPECIAL_FACILITY (
 DROP TABLE IF EXISTS CALL_FORWARDING;
 CREATE TABLE CALL_FORWARDING (
    s_id INTEGER NOT NULL,
-   sf_type int NOT NULL,
-   start_time int NOT NULL,
-   end_time int,
-   numberx VARCHAR(15),
+   sf_type SMALLINT NOT NULL,
+   start_time SMALLINT NOT NULL,
+   end_time SMALLINT,
+   numberx STRING,
    PRIMARY KEY (s_id, sf_type, start_time),
    FOREIGN KEY (s_id, sf_type) REFERENCES SPECIAL_FACILITY(s_id, sf_type)
 );
