e5414101839d05a27c78cf87e0637f88e825da89
David L. Day <dday376@gmail.com>
2013-12-06 19:27:41 -0500

    Changed DDL to uppercase for tpcc-ddl, oracle, mysql, sqlserver, and nuodb.

--- src/com/oltpbenchmark/benchmarks/tpcc/tpcc-oracle-ddl.sql
+++ src/com/oltpbenchmark/benchmarks/tpcc/tpcc-oracle-ddl.sql
@@ -1,185 +1,185 @@
-BEGIN EXECUTE IMMEDIATE 'DROP TABLE customer'; EXCEPTION WHEN OTHERS THEN IF SQLCODE != -942 THEN RAISE; END IF; END;;
-CREATE TABLE customer (
-c_id number
-, c_d_id number
-, c_w_id number
-, c_discount number
-, c_credit char(2)
-, c_last varchar2(16)
-, c_first varchar2(16)
-, c_credit_lim number
-, c_balance number
-, c_ytd_payment number
-, c_payment_cnt number
-, c_delivery_cnt number
-, c_street_1 varchar2(20)
-, c_street_2 varchar2(20)
-, c_city varchar2(20)
-, c_state char(2)
-, c_zip char(9)
-, c_phone char(16)
-, c_since date
-, c_middle char(2)
-, c_data char(500)
+BEGIN EXECUTE IMMEDIATE 'DROP TABLE CUSTOMER'; EXCEPTION WHEN OTHERS THEN IF SQLCODE != -942 THEN RAISE; END IF; END;;
+CREATE TABLE CUSTOMER (
+C_ID NUMBER
+, C_D_ID NUMBER
+, C_W_ID NUMBER
+, C_DISCOUNT NUMBER
+, C_CREDIT CHAR(2)
+, C_LAST VARCHAR2(16)
+, C_FIRST VARCHAR2(16)
+, C_CREDIT_LIM NUMBER
+, C_BALANCE NUMBER
+, C_YTD_PAYMENT NUMBER
+, C_PAYMENT_CNT NUMBER
+, C_DELIVERY_CNT NUMBER
+, C_STREET_1 VARCHAR2(20)
+, C_STREET_2 VARCHAR2(20)
+, C_CITY VARCHAR2(20)
+, C_STATE CHAR(2)
+, C_ZIP CHAR(9)
+, C_PHONE CHAR(16)
+, C_SINCE DATE
+, C_MIDDLE CHAR(2)
+, C_DATA CHAR(500)
 );
 
 
-BEGIN EXECUTE IMMEDIATE 'DROP TABLE district'; EXCEPTION WHEN OTHERS THEN IF SQLCODE != -942 THEN RAISE; END IF; END;;
-CREATE TABLE district (
-  d_id number
-, d_w_id number
-, d_ytd number
-, d_next_o_id number
-, d_tax number
-, d_name varchar2(10)
-, d_street_1 varchar2(20)
-, d_street_2 varchar2(20)
-, d_city varchar2(20)
-, d_state char(2)
-, d_zip char(9)
+BEGIN EXECUTE IMMEDIATE 'DROP TABLE DISTRICT'; EXCEPTION WHEN OTHERS THEN IF SQLCODE != -942 THEN RAISE; END IF; END;;
+CREATE TABLE DISTRICT (
+  D_ID NUMBER
+, D_W_ID NUMBER
+, D_YTD NUMBER
+, D_NEXT_O_ID NUMBER
+, D_TAX NUMBER
+, D_NAME VARCHAR2(10)
+, D_STREET_1 VARCHAR2(20)
+, D_STREET_2 VARCHAR2(20)
+, D_CITY VARCHAR2(20)
+, D_STATE CHAR(2)
+, D_ZIP CHAR(9)
 );
 
-BEGIN EXECUTE IMMEDIATE 'DROP TABLE history'; EXCEPTION WHEN OTHERS THEN IF SQLCODE != -942 THEN RAISE; END IF; END;;
-CREATE TABLE history (
-  h_c_id number
-, h_c_d_id number
-, h_c_w_id number
-, h_d_id number
-, h_w_id number
-, h_date date
-, h_amount number
-, h_data varchar2(24)
+BEGIN EXECUTE IMMEDIATE 'DROP TABLE HISTORY'; EXCEPTION WHEN OTHERS THEN IF SQLCODE != -942 THEN RAISE; END IF; END;;
+CREATE TABLE HISTORY (
+  H_C_ID NUMBER
+, H_C_D_ID NUMBER
+, H_C_W_ID NUMBER
+, H_D_ID NUMBER
+, H_W_ID NUMBER
+, H_DATE DATE
+, H_AMOUNT NUMBER
+, H_DATA VARCHAR2(24)
 );
 
-BEGIN EXECUTE IMMEDIATE 'DROP TABLE item'; EXCEPTION WHEN OTHERS THEN IF SQLCODE != -942 THEN RAISE; END IF; END;;
-CREATE TABLE item (
-  i_id number(6,0)
-, i_name varchar2(24)
-, i_price number
-, i_data varchar2(50)
-, i_im_id number
+BEGIN EXECUTE IMMEDIATE 'DROP TABLE ITEM'; EXCEPTION WHEN OTHERS THEN IF SQLCODE != -942 THEN RAISE; END IF; END;;
+CREATE TABLE ITEM (
+  I_ID NUMBER(6,0)
+, I_NAME VARCHAR2(24)
+, I_PRICE NUMBER
+, I_DATA VARCHAR2(50)
+, I_IM_ID NUMBER
 );
 
-BEGIN EXECUTE IMMEDIATE 'DROP TABLE new_order'; EXCEPTION WHEN OTHERS THEN IF SQLCODE != -942 THEN RAISE; END IF; END;;
-CREATE TABLE new_order (
-  no_w_id number
-, no_d_id number
-, no_o_id number sort
-, constraint nord_uk primary key ( no_w_id, no_d_id, no_o_id)
+BEGIN EXECUTE IMMEDIATE 'DROP TABLE NEW_ORDER'; EXCEPTION WHEN OTHERS THEN IF SQLCODE != -942 THEN RAISE; END IF; END;;
+CREATE TABLE NEW_ORDER (
+  NO_W_ID NUMBER
+, NO_D_ID NUMBER
+, NO_O_ID NUMBER SORT
+, CONSTRAINT NORD_UK PRIMARY KEY ( NO_W_ID, NO_D_ID, NO_O_ID)
 );
 
-BEGIN EXECUTE IMMEDIATE 'DROP TABLE oorder'; EXCEPTION WHEN OTHERS THEN IF SQLCODE != -942 THEN RAISE; END IF; END;;
-CREATE TABLE oorder (
-  o_id number sort
-, o_w_id number
-, o_d_id number
-, o_c_id number
-, o_carrier_id number
-, o_ol_cnt number
-, o_all_local number
-, o_entry_d date
-, constraint ordr_uk primary key ( o_w_id, o_d_id, o_id )
-);
-
-
-BEGIN EXECUTE IMMEDIATE 'DROP TABLE order_line'; EXCEPTION WHEN OTHERS THEN IF SQLCODE != -942 THEN RAISE; END IF; END;;
-CREATE TABLE order_line (
-  ol_w_id number
-, ol_d_id number
-, ol_o_id number sort
-, ol_number number sort
-, ol_i_id number
-, ol_delivery_d date
-, ol_amount number
-, ol_supply_w_id number
-, ol_quantity number
-, ol_dist_info char(24)
-, constraint ordl_uk primary key (ol_w_id, ol_d_id, ol_o_id, ol_number)
+BEGIN EXECUTE IMMEDIATE 'DROP TABLE OORDER'; EXCEPTION WHEN OTHERS THEN IF SQLCODE != -942 THEN RAISE; END IF; END;;
+CREATE TABLE OORDER (
+  O_ID NUMBER SORT
+, O_W_ID NUMBER
+, O_D_ID NUMBER
+, O_C_ID NUMBER
+, O_CARRIER_ID NUMBER
+, O_OL_CNT NUMBER
+, O_ALL_LOCAL NUMBER
+, O_ENTRY_D DATE
+, CONSTRAINT ORDR_UK PRIMARY KEY ( O_W_ID, O_D_ID, O_ID )
 );
 
 
-BEGIN EXECUTE IMMEDIATE 'DROP TABLE stock'; EXCEPTION WHEN OTHERS THEN IF SQLCODE != -942 THEN RAISE; END IF; END;;
-CREATE TABLE stock (
-  s_i_id number
-, s_w_id number
-, s_quantity number
-, s_ytd number
-, s_order_cnt number
-, s_remote_cnt number
-, s_data varchar2(50)
-, s_dist_01 char(24)
-, s_dist_02 char(24)
-, s_dist_03 char(24)
-, s_dist_04 char(24)
-, s_dist_05 char(24)
-, s_dist_06 char(24)
-, s_dist_07 char(24)
-, s_dist_08 char(24)
-, s_dist_09 char(24)
-, s_dist_10 char(24)
+BEGIN EXECUTE IMMEDIATE 'DROP TABLE ORDER_LINE'; EXCEPTION WHEN OTHERS THEN IF SQLCODE != -942 THEN RAISE; END IF; END;;
+CREATE TABLE ORDER_LINE (
+  OL_W_ID NUMBER
+, OL_D_ID NUMBER
+, OL_O_ID NUMBER SORT
+, OL_NUMBER NUMBER SORT
+, OL_I_ID NUMBER
+, OL_DELIVERY_D DATE
+, OL_AMOUNT NUMBER
+, OL_SUPPLY_W_ID NUMBER
+, OL_QUANTITY NUMBER
+, OL_DIST_INFO CHAR(24)
+, CONSTRAINT ORDL_UK PRIMARY KEY (OL_W_ID, OL_D_ID, OL_O_ID, OL_NUMBER)
 );
 
 
-BEGIN EXECUTE IMMEDIATE 'DROP TABLE warehouse'; EXCEPTION WHEN OTHERS THEN IF SQLCODE != -942 THEN RAISE; END IF; END;;
-CREATE TABLE warehouse (
-  w_id number
-, w_ytd number
-, w_tax number
-, w_name varchar2(10)
-, w_street_1 varchar2(20)
-, w_street_2 varchar2(20)
-, w_city varchar2(20)
-, w_state char(2)
-, w_zip char(9)
+BEGIN EXECUTE IMMEDIATE 'DROP TABLE STOCK'; EXCEPTION WHEN OTHERS THEN IF SQLCODE != -942 THEN RAISE; END IF; END;;
+CREATE TABLE STOCK (
+  S_I_ID NUMBER
+, S_W_ID NUMBER
+, S_QUANTITY NUMBER
+, S_YTD NUMBER
+, S_ORDER_CNT NUMBER
+, S_REMOTE_CNT NUMBER
+, S_DATA VARCHAR2(50)
+, S_DIST_01 CHAR(24)
+, S_DIST_02 CHAR(24)
+, S_DIST_03 CHAR(24)
+, S_DIST_04 CHAR(24)
+, S_DIST_05 CHAR(24)
+, S_DIST_06 CHAR(24)
+, S_DIST_07 CHAR(24)
+, S_DIST_08 CHAR(24)
+, S_DIST_09 CHAR(24)
+, S_DIST_10 CHAR(24)
 );
 
 
--- Indexes
+BEGIN EXECUTE IMMEDIATE 'DROP TABLE WAREHOUSE'; EXCEPTION WHEN OTHERS THEN IF SQLCODE != -942 THEN RAISE; END IF; END;;
+CREATE TABLE WAREHOUSE (
+  W_ID NUMBER
+, W_YTD NUMBER
+, W_TAX NUMBER
+, W_NAME VARCHAR2(10)
+, W_STREET_1 VARCHAR2(20)
+, W_STREET_2 VARCHAR2(20)
+, W_CITY VARCHAR2(20)
+, W_STATE CHAR(2)
+, W_ZIP CHAR(9)
+);
 
---Index District
-create unique index idist on district ( d_w_id, d_id )
-  pctfree 5  initrans 3
-  storage ( buffer_pool default )
-  parallel 1
-  compute statistics;
 
---Index customer
-create unique index icust1 on customer ( c_w_id, c_d_id, c_id )
-  pctfree 1  initrans 3
-  storage ( buffer_pool default )
-  parallel 4
-  compute statistics;
+-- INDEXES
 
---Index customer ...
-create unique index icust2 on customer ( c_last, c_w_id, c_d_id, c_id )
-  pctfree 1  initrans 3
-  storage ( buffer_pool default )
-  parallel 4
-  compute statistics;
+--INDEX DISTRICT
+CREATE UNIQUE INDEX IDIST ON DISTRICT ( D_W_ID, D_ID )
+  PCTFREE 5  INITRANS 3
+  STORAGE ( BUFFER_POOL DEFAULT )
+  PARALLEL 1
+  COMPUTE STATISTICS;
 
---PROMPT Creating Primary Key Constraint PRIMARY_2 on table item ... 
-create unique index iitem on item ( i_id )
-  pctfree 5  initrans 4
-  storage ( buffer_pool default )
-  compute statistics;
+--INDEX CUSTOMER
+CREATE UNIQUE INDEX ICUST1 ON CUSTOMER ( C_W_ID, C_D_ID, C_ID )
+  PCTFREE 1  INITRANS 3
+  STORAGE ( BUFFER_POOL DEFAULT )
+  PARALLEL 4
+  COMPUTE STATISTICS;
 
--- Index warehouse 
-create unique index iwarehouse on warehouse ( w_id )
-  pctfree 1  initrans 3
-  storage ( buffer_pool default )
-  parallel 1
-  compute statistics;
+--INDEX CUSTOMER ...
+CREATE UNIQUE INDEX ICUST2 ON CUSTOMER ( C_LAST, C_W_ID, C_D_ID, C_ID )
+  PCTFREE 1  INITRANS 3
+  STORAGE ( BUFFER_POOL DEFAULT )
+  PARALLEL 4
+  COMPUTE STATISTICS;
 
--- Index Stock
-create unique index istock on stock ( s_i_id, s_w_id )
-  pctfree 1  initrans 3
-  storage ( buffer_pool default )
-  parallel 4
-  compute statistics;
+--PROMPT CREATING PRIMARY KEY CONSTRAINT PRIMARY_2 ON TABLE ITEM ... 
+CREATE UNIQUE INDEX IITEM ON ITEM ( I_ID )
+  PCTFREE 5  INITRANS 4
+  STORAGE ( BUFFER_POOL DEFAULT )
+  COMPUTE STATISTICS;
+
+-- INDEX WAREHOUSE 
+CREATE UNIQUE INDEX IWAREHOUSE ON WAREHOUSE ( W_ID )
+  PCTFREE 1  INITRANS 3
+  STORAGE ( BUFFER_POOL DEFAULT )
+  PARALLEL 1
+  COMPUTE STATISTICS;
+
+-- INDEX STOCK
+CREATE UNIQUE INDEX ISTOCK ON STOCK ( S_I_ID, S_W_ID )
+  PCTFREE 1  INITRANS 3
+  STORAGE ( BUFFER_POOL DEFAULT )
+  PARALLEL 4
+  COMPUTE STATISTICS;
   
---Index on oorder...
-create unique index ioorder on oorder ( o_w_id, o_d_id, o_c_id, o_id )
-  parallel 4
-  pctfree 25  initrans 4
-  storage ( buffer_pool default )
-  compute statistics;
+--INDEX ON OORDER...
+CREATE UNIQUE INDEX IOORDER ON OORDER ( O_W_ID, O_D_ID, O_C_ID, O_ID )
+  PARALLEL 4
+  PCTFREE 25  INITRANS 4
+  STORAGE ( BUFFER_POOL DEFAULT )
+  COMPUTE STATISTICS;
--- src/com/oltpbenchmark/benchmarks/tpcc/tpcc-sqlserver-ddl.sql
+++ src/com/oltpbenchmark/benchmarks/tpcc/tpcc-sqlserver-ddl.sql
@@ -1,144 +1,144 @@
---  Drop Existing Tables
-IF OBJECT_ID('warehouse') IS NOT NULL DROP table warehouse;
-IF OBJECT_ID('stock') IS NOT NULL DROP table stock;
-IF OBJECT_ID('order_line') IS NOT NULL DROP table order_line;
-IF OBJECT_ID('oorder') IS NOT NULL DROP table oorder;
-IF OBJECT_ID('new_order') IS NOT NULL DROP table new_order;
-IF OBJECT_ID('item') IS NOT NULL DROP table item;
-IF OBJECT_ID('history') IS NOT NULL DROP table history;
-IF OBJECT_ID('district') IS NOT NULL DROP table district;
-IF OBJECT_ID('customer') IS NOT NULL DROP table customer;
+--  DROP EXISTING TABLES
+IF OBJECT_ID('WAREHOUSE') IS NOT NULL DROP TABLE WAREHOUSE;
+IF OBJECT_ID('STOCK') IS NOT NULL DROP TABLE STOCK;
+IF OBJECT_ID('ORDER_LINE') IS NOT NULL DROP TABLE ORDER_LINE;
+IF OBJECT_ID('OORDER') IS NOT NULL DROP TABLE OORDER;
+IF OBJECT_ID('NEW_ORDER') IS NOT NULL DROP TABLE NEW_ORDER;
+IF OBJECT_ID('ITEM') IS NOT NULL DROP TABLE ITEM;
+IF OBJECT_ID('HISTORY') IS NOT NULL DROP TABLE HISTORY;
+IF OBJECT_ID('DISTRICT') IS NOT NULL DROP TABLE DISTRICT;
+IF OBJECT_ID('CUSTOMER') IS NOT NULL DROP TABLE CUSTOMER;
 
--- Create Tables
+-- CREATE TABLES
 
-CREATE TABLE [dbo].[warehouse](
-	[w_id] [int] NOT NULL,
-	[w_ytd] [decimal](12, 2) NOT NULL,
-	[w_tax] [decimal](4, 4) NOT NULL,
-	[w_name] [varchar](10) NOT NULL,
-	[w_street_1] [varchar](20) NOT NULL,
-	[w_street_2] [varchar](20) NOT NULL,
-	[w_city] [varchar](20) NOT NULL,
-	[w_state] [char](2) NOT NULL,
-	[w_zip] [char](9) NOT NULL,
-	PRIMARY KEY (w_id)
+CREATE TABLE [DBO].[WAREHOUSE](
+	[W_ID] [INT] NOT NULL,
+	[W_YTD] [DECIMAL](12, 2) NOT NULL,
+	[W_TAX] [DECIMAL](4, 4) NOT NULL,
+	[W_NAME] [VARCHAR](10) NOT NULL,
+	[W_STREET_1] [VARCHAR](20) NOT NULL,
+	[W_STREET_2] [VARCHAR](20) NOT NULL,
+	[W_CITY] [VARCHAR](20) NOT NULL,
+	[W_STATE] [CHAR](2) NOT NULL,
+	[W_ZIP] [CHAR](9) NOT NULL,
+	PRIMARY KEY (W_ID)
 );
 
-CREATE TABLE [dbo].[stock](
-	[s_w_id] [int] NOT NULL,
-	[s_i_id] [int] NOT NULL,
-	[s_quantity] [decimal](4, 0) NOT NULL,
-	[s_ytd] [decimal](8, 2) NOT NULL,
-	[s_order_cnt] [int] NOT NULL,
-	[s_remote_cnt] [int] NOT NULL,
-	[s_data] [varchar](50) NOT NULL,
-	[s_dist_01] [char](24) NOT NULL,
-	[s_dist_02] [char](24) NOT NULL,
-	[s_dist_03] [char](24) NOT NULL,
-	[s_dist_04] [char](24) NOT NULL,
-	[s_dist_05] [char](24) NOT NULL,
-	[s_dist_06] [char](24) NOT NULL,
-	[s_dist_07] [char](24) NOT NULL,
-	[s_dist_08] [char](24) NOT NULL,
-	[s_dist_09] [char](24) NOT NULL,
-	[s_dist_10] [char](24) NOT NULL,
-	PRIMARY KEY (s_w_id,s_i_id)
+CREATE TABLE [DBO].[STOCK](
+	[S_W_ID] [INT] NOT NULL,
+	[S_I_ID] [INT] NOT NULL,
+	[S_QUANTITY] [DECIMAL](4, 0) NOT NULL,
+	[S_YTD] [DECIMAL](8, 2) NOT NULL,
+	[S_ORDER_CNT] [INT] NOT NULL,
+	[S_REMOTE_CNT] [INT] NOT NULL,
+	[S_DATA] [VARCHAR](50) NOT NULL,
+	[S_DIST_01] [CHAR](24) NOT NULL,
+	[S_DIST_02] [CHAR](24) NOT NULL,
+	[S_DIST_03] [CHAR](24) NOT NULL,
+	[S_DIST_04] [CHAR](24) NOT NULL,
+	[S_DIST_05] [CHAR](24) NOT NULL,
+	[S_DIST_06] [CHAR](24) NOT NULL,
+	[S_DIST_07] [CHAR](24) NOT NULL,
+	[S_DIST_08] [CHAR](24) NOT NULL,
+	[S_DIST_09] [CHAR](24) NOT NULL,
+	[S_DIST_10] [CHAR](24) NOT NULL,
+	PRIMARY KEY (S_W_ID,S_I_ID)
 );
 
-CREATE TABLE [dbo].[order_line](
-	[ol_w_id] [int] NOT NULL,
-	[ol_d_id] [int] NOT NULL,
-	[ol_o_id] [int] NOT NULL,
-	[ol_number] [int] NOT NULL,
-	[ol_i_id] [int] NOT NULL,
-	[ol_delivery_d] [smalldatetime] NULL,
-	[ol_amount] [decimal](6, 2) NOT NULL,
-	[ol_supply_w_id] [int] NOT NULL,
-	[ol_quantity] [decimal](2, 0) NOT NULL,
-	[ol_dist_info] [char](24) NOT NULL,
-	PRIMARY KEY (ol_w_id,ol_d_id,ol_o_id,ol_number)
+CREATE TABLE [DBO].[ORDER_LINE](
+	[OL_W_ID] [INT] NOT NULL,
+	[OL_D_ID] [INT] NOT NULL,
+	[OL_O_ID] [INT] NOT NULL,
+	[OL_NUMBER] [INT] NOT NULL,
+	[OL_I_ID] [INT] NOT NULL,
+	[OL_DELIVERY_D] [SMALLDATETIME] NULL,
+	[OL_AMOUNT] [DECIMAL](6, 2) NOT NULL,
+	[OL_SUPPLY_W_ID] [INT] NOT NULL,
+	[OL_QUANTITY] [DECIMAL](2, 0) NOT NULL,
+	[OL_DIST_INFO] [CHAR](24) NOT NULL,
+	PRIMARY KEY (OL_W_ID,OL_D_ID,OL_O_ID,OL_NUMBER)
 ); 
 
-CREATE TABLE [dbo].[oorder](
-	[o_w_id] [int] NOT NULL,
-	[o_d_id] [int] NOT NULL,
-	[o_id] [int] NOT NULL,
-	[o_c_id] [int] NOT NULL,
-	[o_carrier_id] [int] NULL,
-	[o_ol_cnt] [decimal](2, 0) NOT NULL,
-	[o_all_local] [decimal](1, 0) NOT NULL,
-	[o_entry_d] [smalldatetime] NULL,
-	PRIMARY KEY (o_w_id,o_d_id,o_id),
-    UNIQUE (o_w_id,o_d_id,o_c_id,o_id)
+CREATE TABLE [DBO].[OORDER](
+	[O_W_ID] [INT] NOT NULL,
+	[O_D_ID] [INT] NOT NULL,
+	[O_ID] [INT] NOT NULL,
+	[O_C_ID] [INT] NOT NULL,
+	[O_CARRIER_ID] [INT] NULL,
+	[O_OL_CNT] [DECIMAL](2, 0) NOT NULL,
+	[O_ALL_LOCAL] [DECIMAL](1, 0) NOT NULL,
+	[O_ENTRY_D] [SMALLDATETIME] NULL,
+	PRIMARY KEY (O_W_ID,O_D_ID,O_ID),
+    UNIQUE (O_W_ID,O_D_ID,O_C_ID,O_ID)
 );
 
-CREATE TABLE [dbo].[new_order](
-	[no_w_id] [int] NOT NULL,
-	[no_d_id] [int] NOT NULL,
-	[no_o_id] [int] NOT NULL,
-	PRIMARY KEY (no_w_id,no_d_id,no_o_id)
+CREATE TABLE [DBO].[NEW_ORDER](
+	[NO_W_ID] [INT] NOT NULL,
+	[NO_D_ID] [INT] NOT NULL,
+	[NO_O_ID] [INT] NOT NULL,
+	PRIMARY KEY (NO_W_ID,NO_D_ID,NO_O_ID)
 );
 
-CREATE TABLE [dbo].[item](
-	[i_id] [int] NOT NULL,
-	[i_name] [varchar](24) NOT NULL,
-	[i_price] [decimal](5, 2) NOT NULL,
-	[i_data] [varchar](50) NOT NULL,
-	[i_im_id] [int] NOT NULL,
-	PRIMARY KEY (i_id)
+CREATE TABLE [DBO].[ITEM](
+	[I_ID] [INT] NOT NULL,
+	[I_NAME] [VARCHAR](24) NOT NULL,
+	[I_PRICE] [DECIMAL](5, 2) NOT NULL,
+	[I_DATA] [VARCHAR](50) NOT NULL,
+	[I_IM_ID] [INT] NOT NULL,
+	PRIMARY KEY (I_ID)
 );
 
-CREATE TABLE [dbo].[history](
-	[h_c_id] [int] NOT NULL,
-	[h_c_d_id] [int] NOT NULL,
-	[h_c_w_id] [int] NOT NULL,
-	[h_d_id] [int] NOT NULL,
-	[h_w_id] [int] NOT NULL,
-	[h_date] [smalldatetime] NULL,
-	[h_amount] [decimal](6, 2) NOT NULL,
-	[h_data] [varchar](24) NOT NULL
+CREATE TABLE [DBO].[HISTORY](
+	[H_C_ID] [INT] NOT NULL,
+	[H_C_D_ID] [INT] NOT NULL,
+	[H_C_W_ID] [INT] NOT NULL,
+	[H_D_ID] [INT] NOT NULL,
+	[H_W_ID] [INT] NOT NULL,
+	[H_DATE] [SMALLDATETIME] NULL,
+	[H_AMOUNT] [DECIMAL](6, 2) NOT NULL,
+	[H_DATA] [VARCHAR](24) NOT NULL
 );
 
-CREATE TABLE [dbo].[district](
-	[d_w_id] [int] NOT NULL,
-	[d_id] [int] NOT NULL,
-	[d_ytd] [decimal](12, 2) NOT NULL,
-	[d_tax] [decimal](4, 4) NOT NULL,
-	[d_next_o_id] [int] NOT NULL,
-	[d_name] [varchar](10) NOT NULL,
-	[d_street_1] [varchar](20) NOT NULL,
-	[d_street_2] [varchar](20) NOT NULL,
-	[d_city] [varchar](20) NOT NULL,
-	[d_state] [char](2) NOT NULL,
-	[d_zip] [char](9) NOT NULL,
-	PRIMARY KEY (d_w_id,d_id)
+CREATE TABLE [DBO].[DISTRICT](
+	[D_W_ID] [INT] NOT NULL,
+	[D_ID] [INT] NOT NULL,
+	[D_YTD] [DECIMAL](12, 2) NOT NULL,
+	[D_TAX] [DECIMAL](4, 4) NOT NULL,
+	[D_NEXT_O_ID] [INT] NOT NULL,
+	[D_NAME] [VARCHAR](10) NOT NULL,
+	[D_STREET_1] [VARCHAR](20) NOT NULL,
+	[D_STREET_2] [VARCHAR](20) NOT NULL,
+	[D_CITY] [VARCHAR](20) NOT NULL,
+	[D_STATE] [CHAR](2) NOT NULL,
+	[D_ZIP] [CHAR](9) NOT NULL,
+	PRIMARY KEY (D_W_ID,D_ID)
 );
 
 
-CREATE TABLE [dbo].[customer](
-	[c_w_id] [int] NOT NULL,
-	[c_d_id] [int] NOT NULL,
-	[c_id] [int] NOT NULL,
-	[c_discount] [decimal](4, 4) NOT NULL,
-	[c_credit] [char](2) NOT NULL,
-	[c_last] [varchar](16) NOT NULL,
-	[c_first] [varchar](16) NOT NULL,
-	[c_credit_lim] [decimal](12, 2) NOT NULL,
-	[c_balance] [decimal](12, 2) NOT NULL,
-	[c_ytd_payment] [real] NOT NULL,
-	[c_payment_cnt] [int] NOT NULL,
-	[c_delivery_cnt] [int] NOT NULL,
-	[c_street_1] [varchar](20) NOT NULL,
-	[c_street_2] [varchar](20) NOT NULL,
-	[c_city] [varchar](20) NOT NULL,
-	[c_state] [char](2) NOT NULL,
-	[c_zip] [char](9) NOT NULL,
-	[c_phone] [char](16) NOT NULL,
-	[c_since] [smalldatetime] NULL,
-	[c_middle] [char](2) NOT NULL,
-	[c_data] [varchar](500) NOT NULL,
-	PRIMARY KEY (c_w_id,c_d_id,c_id)
+CREATE TABLE [DBO].[CUSTOMER](
+	[C_W_ID] [INT] NOT NULL,
+	[C_D_ID] [INT] NOT NULL,
+	[C_ID] [INT] NOT NULL,
+	[C_DISCOUNT] [DECIMAL](4, 4) NOT NULL,
+	[C_CREDIT] [CHAR](2) NOT NULL,
+	[C_LAST] [VARCHAR](16) NOT NULL,
+	[C_FIRST] [VARCHAR](16) NOT NULL,
+	[C_CREDIT_LIM] [DECIMAL](12, 2) NOT NULL,
+	[C_BALANCE] [DECIMAL](12, 2) NOT NULL,
+	[C_YTD_PAYMENT] [REAL] NOT NULL,
+	[C_PAYMENT_CNT] [INT] NOT NULL,
+	[C_DELIVERY_CNT] [INT] NOT NULL,
+	[C_STREET_1] [VARCHAR](20) NOT NULL,
+	[C_STREET_2] [VARCHAR](20) NOT NULL,
+	[C_CITY] [VARCHAR](20) NOT NULL,
+	[C_STATE] [CHAR](2) NOT NULL,
+	[C_ZIP] [CHAR](9) NOT NULL,
+	[C_PHONE] [CHAR](16) NOT NULL,
+	[C_SINCE] [SMALLDATETIME] NULL,
+	[C_MIDDLE] [CHAR](2) NOT NULL,
+	[C_DATA] [VARCHAR](500) NOT NULL,
+	PRIMARY KEY (C_W_ID,C_D_ID,C_ID)
 );
 
--- Create Indexes
-CREATE INDEX IDX_CUSTOMER_NAME ON [dbo].[customer] (c_w_id,c_d_id,c_last,c_first);
+-- CREATE INDEXES
+CREATE INDEX IDX_CUSTOMER_NAME ON [DBO].[CUSTOMER] (C_W_ID,C_D_ID,C_LAST,C_FIRST);
