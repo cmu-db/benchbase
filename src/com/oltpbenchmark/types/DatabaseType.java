3766639fb845c4e056ea99383815118c7ed4e001
Andy Pavlo <pavlo@cs.brown.edu>
2014-07-10 10:01:11 -0400

    Removed debugging statements. Cleaned up a couple of other things...

--- src/com/oltpbenchmark/types/DatabaseType.java
+++ src/com/oltpbenchmark/types/DatabaseType.java
@@ -34,9 +34,7 @@ public enum DatabaseType {
     }
     
     public static DatabaseType get(String name) {
-        System.out.println(name);
         DatabaseType ret = DatabaseType.name_lookup.get(name.toUpperCase());
-        System.out.println(ret);
         return (ret);
     }
 }
