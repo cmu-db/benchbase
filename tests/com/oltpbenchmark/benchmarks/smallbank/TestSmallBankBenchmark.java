f3b20013af368e8882473a1e63ddf9eeeecc8a6b
Andy Pavlo <pavlo@cs.brown.edu>
2016-11-20 21:48:29 -0500

    I've finally finished the port of the SmallBank benchmark in the OLTP-Bench framework. Yes, it took me three years but look I've been busy. I'm trying to run my trap at CMU. Shits been rough.
    I would first like to give a shout out to KB. She keeps me out of trouble.
    
    Second, I want to give a shout out to @eric-haibin-lin. He's been on point this semester with helping build our DBMS. There's no denying that.
    
    Finally, and most importantly, shout out to @michaelcahill for sending me the original source code back in 2010 (or whenever it was). Michael is probably one of the best DB devs on the planet. Dope as fuck.
    
    Ok that's it. The port is done. I've tested it on MySQL and Postgres. That's good enough for me. #41

---
+++ tests/com/oltpbenchmark/benchmarks/smallbank/TestSmallBankBenchmark.java
@@ -0,0 +1,39 @@
+/******************************************************************************
+ *  Copyright 2016 by OLTPBenchmark Project                                   *
+ *                                                                            *
+ *  Licensed under the Apache License, Version 2.0 (the "License");           *
+ *  you may not use this file except in compliance with the License.          *
+ *  You may obtain a copy of the License at                                   *
+ *                                                                            *
+ *    http://www.apache.org/licenses/LICENSE-2.0                              *
+ *                                                                            *
+ *  Unless required by applicable law or agreed to in writing, software       *
+ *  distributed under the License is distributed on an "AS IS" BASIS,         *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ *  See the License for the specific language governing permissions and       *
+ *  limitations under the License.                                            *
+ ******************************************************************************/
+
+
+package com.oltpbenchmark.benchmarks.smallbank;
+
+import com.oltpbenchmark.api.AbstractTestBenchmarkModule;
+import com.oltpbenchmark.benchmarks.smallbank.procedures.*;
+
+public class TestSmallBankBenchmark extends AbstractTestBenchmarkModule<SmallBankBenchmark> {
+	
+    public static final Class<?> PROC_CLASSES[] = {
+        Amalgamate.class,
+        Balance.class,
+        DepositChecking.class,
+        SendPayment.class,
+        TransactSavings.class,
+        WriteCheck.class,
+    };
+    
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp(SmallBankBenchmark.class, PROC_CLASSES);
+	}
+	
+}
