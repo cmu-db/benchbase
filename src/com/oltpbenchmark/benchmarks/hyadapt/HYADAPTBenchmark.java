a919f17e7a334626ace799e13f5902dcaf82c582
Andy Pavlo <pavlo@cs.brown.edu>
2016-12-01 08:34:59 -0500

    Added HYADAPT benchmark from @jarulraj

---
+++ src/com/oltpbenchmark/benchmarks/hyadapt/HYADAPTBenchmark.java
@@ -0,0 +1,92 @@
+/******************************************************************************
+ *  Copyright 2015 by OLTPBenchmark Project                                   *
+ *                                                                            *
+ *  Licensed under the Apache License, Version 2.0 (the "License");           *
+ *  you may not use this file except in compliance with the License.          *
+ *  You may obtain a copy of the License at                                   *
+ *                                                                            *
+ *    http://www.apache.org/licenses/LICENSE-2.0                              *
+ *                                                                            *
+ *  Unless required by applicable law or agreed to in writing, software       *
+ *  distributed under the License is distributed on an "AS IS" BASIS,         *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ *  See the License for the specific language governing permissions and       *
+ *  limitations under the License.                                            *
+ ******************************************************************************/
+
+package com.oltpbenchmark.benchmarks.hyadapt;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.oltpbenchmark.WorkloadConfiguration;
+import com.oltpbenchmark.api.BenchmarkModule;
+import com.oltpbenchmark.api.Loader;
+import com.oltpbenchmark.api.Worker;
+import com.oltpbenchmark.benchmarks.hyadapt.procedures.ReadRecord1;
+import com.oltpbenchmark.catalog.Table;
+import com.oltpbenchmark.util.SQLUtil;
+
+public class HYADAPTBenchmark extends BenchmarkModule {
+    private static final Logger LOG = Logger.getLogger(HYADAPTBenchmark.class);
+
+    public HYADAPTBenchmark(WorkloadConfiguration workConf) {
+        super("hyadapt", workConf, true);
+    }
+
+    @Override
+    protected List<Worker> makeWorkersImpl(boolean verbose) throws IOException {
+        ArrayList<Worker> workers = new ArrayList<Worker>();
+        try {
+            Connection metaConn = this.makeConnection();
+
+            // LOADING FROM THE DATABASE IMPORTANT INFORMATION
+            // LIST OF USERS
+
+            Table t = this.catalog.getTable("HTABLE");
+            assert (t != null) : "Invalid table name '" + t + "' " + this.catalog.getTables();
+            String userCount = SQLUtil.getCountSQL(t);
+            Statement stmt = metaConn.createStatement();
+            ResultSet res = stmt.executeQuery(userCount);
+            int init_record_count = 0;
+            while (res.next()) {
+                init_record_count = res.getInt(1);
+            }
+            assert init_record_count > 0;
+            res.close();
+            //
+            for (int i = 0; i < workConf.getTerminals(); ++i) {
+//                Connection conn = this.makeConnection();
+//                conn.setAutoCommit(false);
+                workers.add(new HYADAPTWorker(i, this, init_record_count + 1));
+            } // FOR
+            metaConn.close();
+            
+            LOG.info("Init Record Count :: " + init_record_count);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        return workers;
+    }
+
+    @Override
+    protected Loader makeLoaderImpl(Connection conn) throws SQLException {
+        return new HYADAPTLoader(this, conn);
+    }
+
+    @Override
+    protected Package getProcedurePackageImpl() {
+        // TODO Auto-generated method stub
+        return ReadRecord1.class.getPackage();
+    }
+
+}
---
+++ src/com/oltpbenchmark/benchmarks/hyadapt/HYADAPTLoader.java
@@ -0,0 +1,93 @@
+/******************************************************************************
+ *  Copyright 2015 by OLTPBenchmark Project                                   *
+ *                                                                            *
+ *  Licensed under the Apache License, Version 2.0 (the "License");           *
+ *  you may not use this file except in compliance with the License.          *
+ *  You may obtain a copy of the License at                                   *
+ *                                                                            *
+ *    http://www.apache.org/licenses/LICENSE-2.0                              *
+ *                                                                            *
+ *  Unless required by applicable law or agreed to in writing, software       *
+ *  distributed under the License is distributed on an "AS IS" BASIS,         *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ *  See the License for the specific language governing permissions and       *
+ *  limitations under the License.                                            *
+ ******************************************************************************/
+
+package com.oltpbenchmark.benchmarks.hyadapt;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import org.apache.log4j.Logger;
+
+import com.oltpbenchmark.api.Loader;
+import com.oltpbenchmark.catalog.Table;
+import com.oltpbenchmark.util.SQLUtil;
+
+import java.util.Random;
+
+public class HYADAPTLoader extends Loader {
+    private static final Logger LOG = Logger.getLogger(HYADAPTLoader.class);
+    private final int num_record;
+    private static final Random rand = new Random();
+
+    public HYADAPTLoader(HYADAPTBenchmark benchmark, Connection c) {
+        super(benchmark, c);
+        this.num_record = (int) Math.round(HYADAPTConstants.RECORD_COUNT * this.scaleFactor);
+        LOG.info("# of RECORDS:  " + this.num_record);        
+    }    
+
+    /**
+     * Returns a pseudo-random number between min and max, inclusive.
+     * The difference between min and max can be at most
+     * <code>Integer.MAX_VALUE - 1</code>.
+     *
+     * @param min Minimum value
+     * @param max Maximum value.  Must be greater than min.
+     * @return Integer between min and max, inclusive.
+     * @see java.util.Random#nextInt(int)
+     */
+    public static int getRandInt() {
+        // nextInt is normally exclusive of the top value,
+        // so add 1 to make it inclusive
+        int min = 0;
+        int max = HYADAPTConstants.RANGE;        
+
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+        return randomNum;
+    }
+    
+    @Override
+    public void load() throws SQLException {
+        Table catalog_tbl = this.getTableCatalog("HTABLE");
+        assert (catalog_tbl != null);
+        
+        String sql = SQLUtil.getInsertSQL(catalog_tbl);
+        PreparedStatement stmt = this.conn.prepareStatement(sql);
+        long total = 0;
+        int batch = 0;
+        for (int i = 0; i < this.num_record; i++) {
+            stmt.setInt(1, i);
+            for (int j = 2; j <= HYADAPTConstants.FIELD_COUNT + 1; j++) {
+                stmt.setInt(j, getRandInt());
+            }
+            stmt.addBatch();
+            total++;
+            if (++batch >= HYADAPTConstants.configCommitCount) {
+                int result[] = stmt.executeBatch();
+                assert (result != null);
+                conn.commit();
+                batch = 0;
+                LOG.info(String.format("Records Loaded %d / %d", total, this.num_record));
+            }
+        } // FOR
+        if (batch > 0) {
+            stmt.executeBatch();
+            LOG.info(String.format("Records Loaded %d / %d", total, this.num_record));
+        }
+        stmt.close();
+        LOG.info("Finished loading " + catalog_tbl.getName());
+    }
+}
