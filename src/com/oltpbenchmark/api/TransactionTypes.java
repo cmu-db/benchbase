9f6451acd70750452023f2d08d41c5756a3a90a6
Andy Pavlo <pavlo@cs.brown.edu>
2016-12-29 15:23:38 -0500

    Forgot to remove the import for SplitIterator

--- src/com/oltpbenchmark/api/TransactionTypes.java
+++ src/com/oltpbenchmark/api/TransactionTypes.java
@@ -21,10 +21,8 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Spliterator;
 
 import org.apache.commons.collections15.map.ListOrderedMap;
-
 
 public class TransactionTypes implements Collection<TransactionType> {
 	
