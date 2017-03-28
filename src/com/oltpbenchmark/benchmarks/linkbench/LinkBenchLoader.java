d514bc7073e8a5d3609da8f769d8d90691b9811f
Daniel Black <daniel.black@au.ibm.com>
2017-03-29 10:23:51 +1100

    linkbench: while load fails may as well do nothing

--- src/com/oltpbenchmark/benchmarks/linkbench/LinkBenchLoader.java
+++ src/com/oltpbenchmark/benchmarks/linkbench/LinkBenchLoader.java
@@ -48,35 +48,7 @@ public class LinkBenchLoader extends Loader<LinkBenchBenchmark> {
 
     @Override
     public void load() throws SQLException {
-        Table catalog_tbl = this.benchmark.getTableCatalog("USERTABLE");
-        assert (catalog_tbl != null);
-        
-        String sql = SQLUtil.getInsertSQL(catalog_tbl);
-        PreparedStatement stmt = this.conn.prepareStatement(sql);
-        long total = 0;
-        int batch = 0;
-        for (int i = 0; i < this.num_record; i++) {
-            stmt.setInt(1, i);
-            for (int j = 2; j <= 11; j++) {
-                stmt.setString(j, TextGenerator.randomStr(rng(), 100));
-            }
-            stmt.addBatch();
-            total++;
-            if (++batch >= LinkBenchConstants.configCommitCount) {
-                int result[] = stmt.executeBatch();
-                assert (result != null);
-                conn.commit();
-                batch = 0;
-                if (LOG.isDebugEnabled())
-                    LOG.debug(String.format("Records Loaded %d / %d", total, this.num_record));
-            }
-        } // FOR
-        if (batch > 0) {
-            stmt.executeBatch();
-            if (LOG.isDebugEnabled())
-                LOG.debug(String.format("Records Loaded %d / %d", total, this.num_record));
-        }
-        stmt.close();
-        if (LOG.isDebugEnabled()) LOG.debug("Finished loading " + catalog_tbl.getName());
+        // NEEDS PORTING
+        // TODO https://github.com/mdcallag/linkbench/blob/master/src/main/java/com/facebook/LinkBench/LinkBenchLoad.java#L99
     }
 }
