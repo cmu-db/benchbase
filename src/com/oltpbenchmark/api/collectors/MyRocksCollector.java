1b01a1ab53f89ca79e2c377e26209af6ba9276d2
Bohan Zhang <bohan@pku.edu.cn>
2017-11-02 16:16:30 -0400

    add additional metrics for myrocks collector

--- src/com/oltpbenchmark/api/collectors/MyRocksCollector.java
+++ src/com/oltpbenchmark/api/collectors/MyRocksCollector.java
@@ -40,6 +40,13 @@ public class MyRocksCollector extends DBCollector {
     private static final String CF_STATS= "select * from information_schema.rocksdb_cfstats;";
 
 
+    private static final String COMPACTION_STATS= "select * from information_schema.rocksdb_compaction_stats;";
+
+
+    private static final String ROCKSDB_DBSTATS= "select * from information_schema.rocksdb_dbstats;";
+
+    private static final String PERF_CONTEXT_GLOBAL= "select * from information_schema.rocksdb_perf_context_global;";
+    
     public MyRocksCollector(String oriDBUrl, String username, String password) {
         try {
             Connection conn = DriverManager.getConnection(oriDBUrl, username, password);
@@ -61,7 +68,7 @@ public class MyRocksCollector extends DBCollector {
             // Collect MyRocks Column Family parameters
             out = s.executeQuery(CF_OPTIONS);
             while(out.next()){
-                dbParameters.put(out.getString(1).toLowerCase() + " | " + out.getString(2).toLowerCase(), out.getString(3));
+                dbParameters.put("cf_option: " + "cf_name=" + out.getString(1).toLowerCase() + " , " + "type=" + out.getString(2).toLowerCase(), out.getString(3));
             }
 
             // Collect DBMS internal metrics
@@ -73,8 +80,28 @@ public class MyRocksCollector extends DBCollector {
             // Collect MyRocks Column Family statistics 
             out = s.executeQuery(CF_STATS);
             while(out.next()){
-                dbMetrics.put(out.getString(1).toLowerCase() + " | " + out.getString(2).toLowerCase(), out.getString(3));
+                dbMetrics.put("cf_stats: "+ "cf_name=" + out.getString(1).toLowerCase() + " , " + "type="+ out.getString(2).toLowerCase(), out.getString(3));
             }            
+
+            // Collect Compaction Statistics
+            out = s.executeQuery(COMPACTION_STATS);
+            while(out.next()){
+                dbMetrics.put("cf_compaction_stats: "+ "cf_name=" + out.getString(1).toLowerCase() + " , " + "level=" + out.getString(2).toLowerCase() + " , " +"type=" + out.getString(3).toLowerCase(), out.getString(4));
+            }            
+
+            // Collect DB statistics
+            out = s.executeQuery(ROCKSDB_DBSTATS);
+            while (out.next()) {
+            	dbMetrics.put(out.getString(1).toLowerCase(), out.getString(2));
+            }    
+
+            // Collect Global Performance Context Statistics 
+            out = s.executeQuery(PERF_CONTEXT_GLOBAL);
+            while (out.next()) {
+            	dbMetrics.put(out.getString(1).toLowerCase(), out.getString(2));
+            }
+            // Collect Perf Context table? 
+            // index, db, table statistic ? 
 
 
 
