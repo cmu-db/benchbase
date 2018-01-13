1697580251df1273777f201e9d6e8ceda49757cc
Monica Pardeshi <monica.pardeshi@gmail.com>
2018-01-13 00:05:46 -0600

    add constants

---
+++ src/com/oltpbenchmark/benchmarks/tpcds/TPCDSConstants.java
@@ -0,0 +1,28 @@
+package com.oltpbenchmark.benchmarks.tpcds;
+
+public abstract class TPCDSConstants {
+    public static final String TABLE_CALLCENTER = "call_center";
+    public static final String TABLE_CATALOGPAGE = "catalog_page";
+    public static final String TABLE_CATALOGRETURNS = "catalog_returns";
+    public static final String TABLE_CATALOGSALES = "catalog_sales";
+    public static final String TABLE_CUSTOMER = "customer";
+    public static final String TABLE_CUSTOMERADDRESS = "customer_address";
+    public static final String TABLE_CUSTOMERDEMO = "customer_demographics";
+    public static final String TABLE_DATEDIM = "date_dim";
+    public static final String TABLE_HOUSEHOLDDEMO = "household_demographics";
+    public static final String TABLE_INCOMEBAND = "income_band";
+    public static final String TABLE_INVENTORY = "inventory";
+    public static final String TABLE_ITEM = "item";
+    public static final String TABLE_PROMOTION = "promotion";
+    public static final String TABLE_REASON = "reason";
+    public static final String TABLE_SHIPMODE = "ship_mode";
+    public static final String TABLE_STORE = "store";
+    public static final String TABLE_STORERETURNS = "store_returns";
+    public static final String TABLE_STORESALES = "store_sales";
+    public static final String TABLE_TIMEDIM = "time_dim";
+    public static final String TABLE_WAREHOUSE = "warehouse";
+    public static final String TABLE_WEBPAGE = "web_page";
+    public static final String TABLE_WEBRETURNS = "web_returns";
+    public static final String TABLE_WEBSALES = "web_sales";
+    public static final String TABLE_WEBSITE = "web_site";
+}
--- src/com/oltpbenchmark/benchmarks/tpcds/TPCDSLoader.java
+++ src/com/oltpbenchmark/benchmarks/tpcds/TPCDSLoader.java
@@ -1,4 +1,26 @@
 package com.oltpbenchmark.benchmarks.tpcds;
 
-public class TPCDSLoader {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.log4j.Logger;
+
+import com.oltpbenchmark.api.Loader;
+import com.oltpbenchmark.api.Loader.LoaderThread;
+
+public class TPCDSLoader extends Loader<TPCDSBenchmark> {
+    private static final Logger LOG = Logger.getLogger(TPCDSLoader.class);
+
+
 }
