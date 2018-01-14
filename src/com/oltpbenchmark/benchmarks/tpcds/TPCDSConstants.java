c0343c9cdbf614bb4af7967cd16ab5febcad778e
Monica Pardeshi <monica.pardeshi@gmail.com>
2018-01-13 20:24:56 -0600

    add function to parse data file

--- src/com/oltpbenchmark/benchmarks/tpcds/TPCDSConstants.java
+++ src/com/oltpbenchmark/benchmarks/tpcds/TPCDSConstants.java
@@ -1,28 +1,32 @@
 package com.oltpbenchmark.benchmarks.tpcds;
 
 public abstract class TPCDSConstants {
-    public static final String TABLE_CALLCENTER = "call_center";
-    public static final String TABLE_CATALOGPAGE = "catalog_page";
-    public static final String TABLE_CATALOGRETURNS = "catalog_returns";
-    public static final String TABLE_CATALOGSALES = "catalog_sales";
-    public static final String TABLE_CUSTOMER = "customer";
-    public static final String TABLE_CUSTOMERADDRESS = "customer_address";
-    public static final String TABLE_CUSTOMERDEMO = "customer_demographics";
-    public static final String TABLE_DATEDIM = "date_dim";
-    public static final String TABLE_HOUSEHOLDDEMO = "household_demographics";
-    public static final String TABLE_INCOMEBAND = "income_band";
-    public static final String TABLE_INVENTORY = "inventory";
-    public static final String TABLE_ITEM = "item";
-    public static final String TABLE_PROMOTION = "promotion";
-    public static final String TABLE_REASON = "reason";
-    public static final String TABLE_SHIPMODE = "ship_mode";
-    public static final String TABLE_STORE = "store";
-    public static final String TABLE_STORERETURNS = "store_returns";
-    public static final String TABLE_STORESALES = "store_sales";
-    public static final String TABLE_TIMEDIM = "time_dim";
-    public static final String TABLE_WAREHOUSE = "warehouse";
-    public static final String TABLE_WEBPAGE = "web_page";
-    public static final String TABLE_WEBRETURNS = "web_returns";
-    public static final String TABLE_WEBSALES = "web_sales";
-    public static final String TABLE_WEBSITE = "web_site";
+    public static final String TABLENAME_CALLCENTER = "call_center";
+    public static final String TABLENAME_CATALOGPAGE = "catalog_page";
+    public static final String TABLENAME_CATALOGRETURNS = "catalog_returns";
+    public static final String TABLENAME_CATALOGSALES = "catalog_sales";
+    public static final String TABLENAME_CUSTOMER = "customer";
+    public static final String TABLENAME_CUSTOMERADDRESS = "customer_address";
+    public static final String TABLENAME_CUSTOMERDEMO = "customer_demographics";
+    public static final String TABLENAME_DATEDIM = "date_dim";
+    public static final String TABLENAME_HOUSEHOLDDEMO = "household_demographics";
+    public static final String TABLENAME_INCOMEBAND = "income_band";
+    public static final String TABLENAME_INVENTORY = "inventory";
+    public static final String TABLENAME_ITEM = "item";
+    public static final String TABLENAME_PROMOTION = "promotion";
+    public static final String TABLENAME_REASON = "reason";
+    public static final String TABLENAME_SHIPMODE = "ship_mode";
+    public static final String TABLENAME_STORE = "store";
+    public static final String TABLENAME_STORERETURNS = "store_returns";
+    public static final String TABLENAME_STORESALES = "store_sales";
+    public static final String TABLENAME_TIMEDIM = "time_dim";
+    public static final String TABLENAME_WAREHOUSE = "warehouse";
+    public static final String TABLENAME_WEBPAGE = "web_page";
+    public static final String TABLENAME_WEBRETURNS = "web_returns";
+    public static final String TABLENAME_WEBSALES = "web_sales";
+    public static final String TABLENAME_WEBSITE = "web_site";
+
+    public static final int CALLCENTERS = 0;
+    public static final int STORES = 0;
+    public static final int WAREHOUSES = 0;
 }
--- src/com/oltpbenchmark/benchmarks/tpcds/TPCDSLoader.java
+++ src/com/oltpbenchmark/benchmarks/tpcds/TPCDSLoader.java
@@ -18,9 +18,199 @@ import org.apache.log4j.Logger;
 
 import com.oltpbenchmark.api.Loader;
 import com.oltpbenchmark.api.Loader.LoaderThread;
+import com.oltpbenchmark.catalog.Table;
+import com.oltpbenchmark.util.SQLUtil;
 
 public class TPCDSLoader extends Loader<TPCDSBenchmark> {
     private static final Logger LOG = Logger.getLogger(TPCDSLoader.class);
 
+    private static enum CastTypes { LONG, DOUBLE, STRING, DATE };
 
+    public TPCDSLoader(TPCDSBenchmark benchmark, Connection c) {
+        super(benchmark, c);
+    }
+
+    @Override
+    public List<LoaderThread> createLoaderThreads() throws SQLException {
+        return null;
+    }
+
+    private String getFileFormat(){
+        String format = workConf.getXmlConfig().getString("fileFormat");
+            /*
+               Previouse configuration migh not have a fileFormat and assume
+                that the files are csv.
+            */
+        if (format == null) return "csv";
+
+        if((!"csv".equals(format) && !"tbl".equals(format))){
+            throw new IllegalArgumentException("Configuration doesent"
+                    + " have a valid fileFormat");
+        }
+        return format;
+    }
+
+    private Pattern getFormatPattern(String format){
+
+        if("csv".equals(format)){
+            // The following pattern parses the lines by commas, except for
+            // ones surrounded by double-quotes. Further, strings that are
+            // double-quoted have the quotes dropped (we don't need them).
+            return  Pattern.compile("\\s*(\"[^\"]*\"|[^,]*)\\s*,?");
+        }else{
+            return Pattern.compile("[^\\|]*\\|");
+        }
+    }
+
+    private int getFormatGroup(String format){
+        if("csv".equals(format)){
+            return  1;
+        }else{
+            return 0;
+        }
+    }
+
+    protected void transRollback(Connection conn) {
+        try {
+            conn.rollback();
+        } catch (SQLException se) {
+            LOG.debug(se.getMessage());
+        }
+    }
+
+    protected void transCommit(Connection conn) {
+        try {
+            conn.commit();
+        } catch (SQLException se) {
+            LOG.debug(se.getMessage());
+            transRollback(conn);
+        }
+    }
+
+    public void loadData(Connection conn, String table, PreparedStatement ps, CastTypes[] types, int batchSize) {
+        BufferedReader br = null;
+        int recordsRead = 0;
+        try {
+            String format = getFileFormat();
+            File file = new File(workConf.getDataDir()
+                    , table + "."
+                    + format);
+            br = new BufferedReader(new FileReader(file));
+            String line;
+            Pattern pattern = getFormatPattern(format);
+            int group = getFormatGroup(format);
+            Matcher matcher;
+            while ((line = br.readLine()) != null) {
+                matcher = pattern.matcher(line);
+                try {
+                    for (int i = 0; i < types.length; ++i) {
+                        matcher.find();
+                        String field = matcher.group(group);
+                        if (field.charAt(0) == '\"') {
+                            field = field.substring(1, field.length() - 1);
+                        }
+
+                        if(group==0){
+                            field = field.substring(0, field.length() -1);
+                        }
+
+                        switch(types[i]) {
+                            case DOUBLE:
+                                ps.setDouble(i+1, Double.parseDouble(field));
+                                break;
+                            case LONG:
+                                ps.setLong(i+1, Long.parseLong(field));
+                                break;
+                            case STRING:
+                                ps.setString(i+1, field);
+                                break;
+                            case DATE:
+                                // Four possible formats for date
+                                // yyyy-mm-dd
+                                Pattern isoFmt = Pattern.compile("^\\s*(\\d{4})-(\\d{2})-(\\d{2})\\s*$");
+                                Matcher isoMatcher = isoFmt.matcher(field);
+                                // yyyymmdd
+                                Pattern nondelimFmt = Pattern.compile("^\\s*(\\d{4})(\\d{2})(\\d{2})\\s*$");
+                                Matcher nondelimMatcher = nondelimFmt.matcher(field);
+                                // mm/dd/yyyy
+                                Pattern usaFmt = Pattern.compile("^\\s*(\\d{2})/(\\d{2})/(\\d{4})\\s*$");
+                                Matcher usaMatcher = usaFmt.matcher(field);
+                                // dd.mm.yyyy
+                                Pattern eurFmt = Pattern.compile("^\\s*(\\d{2})\\.(\\d{2})\\.(\\d{4})\\s*$");
+                                Matcher eurMatcher = eurFmt.matcher(field);
+
+                                String isoFmtDate;
+                                java.sql.Date fieldAsDate = null;
+                                if (isoMatcher.find()) {
+                                    fieldAsDate = java.sql.Date.valueOf(field);
+                                }
+                                else if (nondelimMatcher.find()) {
+                                    isoFmtDate = nondelimMatcher.group(1) + "-"
+                                                + nondelimMatcher.group(2) + "-"
+                                                + nondelimMatcher.group(3);
+                                    fieldAsDate = java.sql.Date.valueOf(isoFmtDate);
+                                }
+                                else if (usaMatcher.find()) {
+                                    isoFmtDate = usaMatcher.group(3) + "-"
+                                                + usaMatcher.group(1) + "-"
+                                                + usaMatcher.group(2);
+                                    fieldAsDate = java.sql.Date.valueOf(isoFmtDate);
+                                }
+                                else if (eurMatcher.find()) {
+                                    isoFmtDate = eurMatcher.group(3) + "-"
+                                            + eurMatcher.group(2) + "-"
+                                            + eurMatcher.group(1);
+                                    fieldAsDate = java.sql.Date.valueOf(isoFmtDate);
+                                }
+                                else {
+                                    throw new RuntimeException("Unrecognized date \""
+                                            + field + "\" in CSV file: "
+                                            + file.getAbsolutePath());
+                                }
+                                ps.setDate(i+1, fieldAsDate, null);
+                                break;
+                            default:
+                                throw new RuntimeException("Unrecognized type for prepared statement");
+                        }
+                    } // FOR
+
+                } catch(IllegalStateException e) {
+                    // This happens if there wasn't a match against the regex.
+                    LOG.error("Invalid CSV file: " + file.getAbsolutePath());
+                }
+
+                // batch stuff
+            }
+            // batch stuff at eof
+        } catch (SQLException se) {
+            LOG.debug(se.getMessage());
+            se = se.getNextException();
+            LOG.debug(se.getMessage());
+            transRollback(conn);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+            transRollback(conn);
+        } finally {
+            if (br != null){
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        // close connection, add rows
+    }
+
+    private void loadCallCenters(Connection conn) throws SQLException {
+        Table catalog_tbl = this.benchmark.getTableCatalog(TPCDSConstants.TABLENAME_CALLCENTER);
+        assert (catalog_tbl != null);
+
+        String sql = SQLUtil.getInsertSQL(catalog_tbl, this.getDatabaseType());
+        PreparedStatement callCenterInsert = conn.prepareStatement(sql);
+
+
+    }
 }
