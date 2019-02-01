0f249ecdf1649acf02c2074031f7ba2d7eb6f5ff
Tim Veil <tjveil@gmail.com>
2019-01-31 19:19:47 -0500

    closing statment correctly

--- src/main/java/com/oltpbenchmark/benchmarks/resourcestresser/ResourceStresserLoader.java
+++ src/main/java/com/oltpbenchmark/benchmarks/resourcestresser/ResourceStresserLoader.java
@@ -60,41 +60,47 @@ public class ResourceStresserLoader extends Loader<ResourceStresserBenchmark> {
         Table catalog_tbl = this.benchmark.getTableCatalog(tableName);
         assert (catalog_tbl != null);
 
-        if (LOG.isDebugEnabled()) LOG.debug("Start loading " + tableName);
-        String sql = SQLUtil.getInsertSQL(catalog_tbl, this.getDatabaseType());
-        PreparedStatement stmt = conn.prepareStatement(sql);
-        int batch = 0;
-        int i;
-        for (i = 0; i < this.numEmployees; ++i) {
-            stmt.setInt(1, i);
-            if (tableName.equals(ResourceStresserConstants.TABLENAME_CPUTABLE)) {
-                stmt.setString(2, TextGenerator.randomStr(rng(), ResourceStresserConstants.STRING_LENGTH));
-            } else if (tableName.equals(ResourceStresserConstants.TABLENAME_IOTABLE)) {
-                for (int j = 2; j <= catalog_tbl.getColumnCount(); ++j) {
-                    stmt.setString(j, TextGenerator.randomStr(rng(), ResourceStresserConstants.STRING_LENGTH));
-                }
-            } else {
-                assert (tableName.equals(ResourceStresserConstants.TABLENAME_LOCKTABLE) ||
-                        tableName.equals(ResourceStresserConstants.TABLENAME_IOTABLESMALLROW));
-                stmt.setInt(2, rng().nextInt());
-            }
-
-            stmt.addBatch();
-            if (++batch >= ResourceStresserConstants.COMMIT_BATCH_SIZE) {
-                int result[] = stmt.executeBatch();
-                assert (result != null);
-                batch = 0;
-                if (LOG.isDebugEnabled())
-                    LOG.debug(String.format("Records Loaded %d / %d", i + 1, this.numEmployees));
-            }
-        } // FOR
-        if (batch > 0) {
-            stmt.executeBatch();
-            if (LOG.isDebugEnabled())
-                LOG.debug(String.format("Records Loaded %d / %d", i, this.numEmployees));
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Start loading " + tableName);
         }
-        stmt.close();
-        if (LOG.isDebugEnabled()) LOG.debug("Finished loading " + tableName);
+        String sql = SQLUtil.getInsertSQL(catalog_tbl, this.getDatabaseType());
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            int batch = 0;
+            int i;
+            for (i = 0; i < this.numEmployees; ++i) {
+                stmt.setInt(1, i);
+                if (tableName.equals(ResourceStresserConstants.TABLENAME_CPUTABLE)) {
+                    stmt.setString(2, TextGenerator.randomStr(rng(), ResourceStresserConstants.STRING_LENGTH));
+                } else if (tableName.equals(ResourceStresserConstants.TABLENAME_IOTABLE)) {
+                    for (int j = 2; j <= catalog_tbl.getColumnCount(); ++j) {
+                        stmt.setString(j, TextGenerator.randomStr(rng(), ResourceStresserConstants.STRING_LENGTH));
+                    }
+                } else {
+                    assert (tableName.equals(ResourceStresserConstants.TABLENAME_LOCKTABLE) ||
+                            tableName.equals(ResourceStresserConstants.TABLENAME_IOTABLESMALLROW));
+                    stmt.setInt(2, rng().nextInt());
+                }
+
+                stmt.addBatch();
+                if (++batch >= ResourceStresserConstants.COMMIT_BATCH_SIZE) {
+                    int result[] = stmt.executeBatch();
+                    assert (result != null);
+                    batch = 0;
+                    if (LOG.isDebugEnabled()) {
+                        LOG.debug(String.format("Records Loaded %d / %d", i + 1, this.numEmployees));
+                    }
+                }
+            } // FOR
+            if (batch > 0) {
+                stmt.executeBatch();
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug(String.format("Records Loaded %d / %d", i, this.numEmployees));
+                }
+            }
+        }
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Finished loading " + tableName);
+        }
         return;
     }
 
