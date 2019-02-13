9a630a4edc17834cfa7e282c8d51bfd0965c5d27
Tim Veil <tjveil@gmail.com>
2019-02-12 20:54:10 -0500

    fixing smallbank

--- src/main/java/com/oltpbenchmark/benchmarks/smallbank/SmallBankWorker.java
+++ src/main/java/com/oltpbenchmark/benchmarks/smallbank/SmallBankWorker.java
@@ -121,7 +121,6 @@ public class SmallBankWorker extends Worker<SmallBankBenchmark> {
             this.procWriteCheck.run(conn, custName, SmallBankConstants.PARAM_WRITE_CHECK_AMOUNT);
 
         }
-        conn.commit();
 
         return TransactionStatus.SUCCESS;
     }
