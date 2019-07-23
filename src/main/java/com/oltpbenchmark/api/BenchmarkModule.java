8c359b3d410c9eb195ea796f205143d6749da903
Tim Veil <tjveil@gmail.com>
2019-07-23 10:21:36 -0400

    pulling pool size from config

--- src/main/java/com/oltpbenchmark/api/BenchmarkModule.java
+++ src/main/java/com/oltpbenchmark/api/BenchmarkModule.java
@@ -90,8 +90,6 @@ public abstract class BenchmarkModule {
 
     private HikariDataSource dataSource = null;
 
-    private static final Integer MAX_POOL_SIZE = 50;
-
     public BenchmarkModule(WorkloadConfiguration workConf, boolean withCatalog) {
 
 
@@ -103,7 +101,7 @@ public abstract class BenchmarkModule {
         config.setJdbcUrl(workConf.getDBConnection());
         config.setUsername(workConf.getDBUsername());
         config.setPassword(workConf.getDBPassword());
-        config.setMaximumPoolSize(MAX_POOL_SIZE);
+        config.setMaximumPoolSize(workConf.getDBPoolSize());
 
         dataSource = new HikariDataSource(config);
     }
