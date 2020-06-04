a097483644e67716c1c3d84224e59cd0d48dbe37
Tim Veil <3260845+timveil@users.noreply.github.com>
2020-06-04 12:48:37 -0400

    chbenchmark mysql is good

--- src/main/resources/benchmarks/chbenchmark/ddl-mysql.sql
+++ src/main/resources/benchmarks/chbenchmark/ddl-mysql.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS region CASCADE;
-DROP TABLE IF EXISTS nation CASCADE;
 DROP TABLE IF EXISTS supplier CASCADE;
+DROP TABLE IF EXISTS nation CASCADE;
+DROP TABLE IF EXISTS region CASCADE;
 
 CREATE TABLE region (
     r_regionkey int       NOT NULL,
@@ -12,8 +12,9 @@ CREATE TABLE region (
 CREATE TABLE nation (
     n_nationkey int       NOT NULL,
     n_name      char(25)  NOT NULL,
-    n_regionkey int       NOT NULL REFERENCES region (r_regionkey) ON DELETE CASCADE,
+    n_regionkey int       NOT NULL,
     n_comment   char(152) NOT NULL,
+    FOREIGN KEY (n_regionkey) REFERENCES region (r_regionkey) ON DELETE CASCADE,
     PRIMARY KEY (n_nationkey)
 );
 
@@ -21,9 +22,10 @@ CREATE TABLE supplier (
     su_suppkey   int            NOT NULL,
     su_name      char(25)       NOT NULL,
     su_address   varchar(40)    NOT NULL,
-    su_nationkey int            NOT NULL REFERENCES nation (n_nationkey) ON DELETE CASCADE,
+    su_nationkey int            NOT NULL,
     su_phone     char(15)       NOT NULL,
     su_acctbal   numeric(12, 2) NOT NULL,
     su_comment   char(101)      NOT NULL,
+    FOREIGN KEY (su_nationkey) REFERENCES nation (n_nationkey) ON DELETE CASCADE,
     PRIMARY KEY (su_suppkey)
 );
