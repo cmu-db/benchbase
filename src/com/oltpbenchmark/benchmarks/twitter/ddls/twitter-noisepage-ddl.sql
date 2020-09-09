dc32a26a2dc308c3ca75a7176b997d3a7b30b5c2
Andy Pavlo <pavlo@cs.brown.edu>
2020-09-09 14:49:18 -0400

    Fix Twitter Postgres/NoisePage DDL files

--- src/com/oltpbenchmark/benchmarks/twitter/ddls/twitter-noisepage-ddl.sql
+++ src/com/oltpbenchmark/benchmarks/twitter/ddls/twitter-noisepage-ddl.sql
@@ -1,12 +1,10 @@
--- DDL Script converted from Mysql Twitter dump
-
 DROP TABLE IF EXISTS user_profiles CASCADE;
 CREATE TABLE user_profiles (
-  uid int NOT NULL DEFAULT '0',
+  uid int NOT NULL,
   name varchar(255) DEFAULT NULL,
   email varchar(255) DEFAULT NULL,
   partitionid int DEFAULT NULL,
-  partitionid2 smallint DEFAULT NULL,
+  partitionid2 int DEFAULT NULL,
   followers int DEFAULT NULL,
   PRIMARY KEY (uid)
 );
@@ -15,36 +13,34 @@ CREATE INDEX IDX_USER_PARTITION ON user_profiles (partitionid);
 
 DROP TABLE IF EXISTS followers;
 CREATE TABLE followers (
-  f1 int NOT NULL DEFAULT '0' REFERENCES user_profiles (uid),
-  f2 int NOT NULL DEFAULT '0' REFERENCES user_profiles (uid),
+  f1 int NOT NULL,
+  f2 int NOT NULL,
   PRIMARY KEY (f1,f2)
 );
 
 DROP TABLE IF EXISTS follows;
 CREATE TABLE follows (
-  f1 int NOT NULL DEFAULT '0',
-  f2 int NOT NULL DEFAULT '0',
+  f1 int NOT NULL,
+  f2 int NOT NULL,
   PRIMARY KEY (f1,f2)
 );
 
--- TODO: id AUTO_INCREMENT
 DROP TABLE IF EXISTS tweets;
 CREATE TABLE tweets (
   id bigint NOT NULL,
-  uid int NOT NULL REFERENCES user_profiles (uid),
+  uid int NOT NULL,
   text char(140) NOT NULL,
-  createdate timestamp DEFAULT NULL,
+  createdate date DEFAULT NULL,
   PRIMARY KEY (id)
 );
 CREATE INDEX IDX_TWEETS_UID ON tweets (uid);
 
--- TODO: id AUTO_INCREMENT
 DROP TABLE IF EXISTS added_tweets;
 CREATE TABLE added_tweets (
-  id serial,
-  uid int NOT NULL REFERENCES user_profiles (uid),
+  id bigint NOT NULL,
+  uid int NOT NULL,
   text char(140) NOT NULL,
-  createdate timestamp DEFAULT NULL,
+  createdate date DEFAULT NULL,
   PRIMARY KEY (id)
 );
 CREATE INDEX IDX_ADDED_TWEETS_UID ON added_tweets (uid);
