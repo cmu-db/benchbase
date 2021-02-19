a85726ab68472bc0e2ea8967ed6de4562017a730
Andy Pavlo <pavlo@cs.brown.edu>
2021-02-18 21:18:08 -0500

    Fix for null phase bug in Worker. Props to @bialesdaniel for pointing this out. Shine on you yeti of a man! (#352)

--- src/com/oltpbenchmark/api/Worker.java
+++ src/com/oltpbenchmark/api/Worker.java
@@ -329,7 +329,9 @@ public abstract class Worker<T extends BenchmarkModule> implements Runnable {
                     // changed, otherwise we're recording results for a query
                     // that either started during the warmup phase or ended
                     // after the timer went off.
-                    if (preState == State.MEASURE && type != null && this.wrkldState.getCurrentPhase().id == phase.id) {
+                    if (preState == State.MEASURE && type != null &&
+                        this.wrkldState.getCurrentPhase() != null &&
+                        phase != null && this.wrkldState.getCurrentPhase().id == phase.id) {
                         latencies.addLatency(type.getId(), start, end, this.id, phase.id);
                         intervalRequests.incrementAndGet();
                     }
