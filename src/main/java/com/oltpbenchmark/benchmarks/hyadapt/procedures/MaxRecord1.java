63ab9b68b1abf2c6f913723896a5b7722627f028
Wan Shen Lim <wanshen.lim@gmail.com>
2021-07-24 02:04:08 -0400

    Fix HYADAPT to be lowercase.

--- src/main/java/com/oltpbenchmark/benchmarks/hyadapt/procedures/MaxRecord1.java
+++ src/main/java/com/oltpbenchmark/benchmarks/hyadapt/procedures/MaxRecord1.java
@@ -32,7 +32,7 @@ public class MaxRecord1 extends Procedure {
 
     public final SQLStmt maxStmt = new SQLStmt(
             "SELECT MAX(FIELD198), MAX(FIELD206), MAX(FIELD169), MAX(FIELD119), MAX(FIELD9), MAX(FIELD220), MAX(FIELD2), MAX(FIELD230), MAX(FIELD212), MAX(FIELD164), MAX(FIELD111), MAX(FIELD136), MAX(FIELD106), MAX(FIELD8), MAX(FIELD112), MAX(FIELD4), MAX(FIELD234), MAX(FIELD147), MAX(FIELD35), MAX(FIELD114), MAX(FIELD89), MAX(FIELD127), MAX(FIELD144), MAX(FIELD71), MAX(FIELD186) "
-                    + "FROM HTABLE WHERE FIELD1>?");
+                    + "FROM htable WHERE FIELD1>?");
 
     public void run(Connection conn, int keyname) throws SQLException {
         int max = -1;
