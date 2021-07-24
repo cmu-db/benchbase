b9ff8e864f26953c46bcb95160297b81cf332a9d
Wan Shen Lim <wanshen.lim@gmail.com>
2021-07-23 23:48:03 -0400

    Improve DBMS header reporting.

--- scripts/result_aggregator.py
+++ scripts/result_aggregator.py
@@ -1,5 +1,5 @@
 """
-python3 result_aggregator.py path_to_results_with_summary_json
+python3 result_aggregator.py path_to_results_with_summary_json dbms
 
 Produces a nicely formatted message for GitHub Actions.
 """
@@ -12,17 +12,19 @@ import sys
 
 if __name__ == '__main__':
   os.chdir(sys.argv[1])
-  dbms_version = ''
+  dbms_header = None
   output = []
   for summary_json_file in sorted(glob.glob("*.summary.json")):
     with open(summary_json_file) as summary_json:
       data = json.load(summary_json)
-      dbms = str(data['DBMS Version']).strip()
+      dbms_type = str(data['DBMS Type']).strip()
+      dbms_version = str(data['DBMS Version']).strip()
       benchmark = str(data['Benchmark Type']).strip()
       throughput = str(data['Throughput (requests/second)']).strip()
-      assert dbms_version == '' or dbms_version == dbms
-      dbms_version = dbms
+      candidate_dbms_header = f'{dbms_type} : {dbms_version}'
+      assert dbms_header == None or dbms_header == candidate_dbms_header
+      dbms_header = candidate_dbms_header
       output.append((benchmark, throughput))
-  print(dbms_version, end='<br>')
+  print(dbms_header, end='<br><br>')
   for benchmark, throughput in output:
     print(benchmark + ', ' + throughput, end='<br>')
