46eb27333232bbe7d8054bb98a29349e4576cbbc
Wan Shen Lim <wanshen.lim@gmail.com>
2021-08-10 12:20:52 -0400

    Fix formatting.

--- src/test/java/com/oltpbenchmark/util/TestCompositeIdRange.java
+++ src/test/java/com/oltpbenchmark/util/TestCompositeIdRange.java
@@ -18,6 +18,7 @@
 package com.oltpbenchmark.util;
 
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 import junit.framework.TestCase;
@@ -49,14 +50,14 @@ public class TestCompositeIdRange {
 
         @Override
         public void decode(long composite_id) {
-            long values[] = super.decode(composite_id, this.COMPOSITE_BITS,this. COMPOSITE_POWS);
-            this.field1 = (int)values[0];
-            this.field2 = (int)values[1];
+            long values[] = super.decode(composite_id, this.COMPOSITE_BITS, this.COMPOSITE_POWS);
+            this.field1 = (int) values[0];
+            this.field2 = (int) values[1];
         }
 
         @Override
         public long[] toArray() {
-            return (new long[]{ this.field1, this.field2 });
+            return (new long[]{this.field1, this.field2});
         }
 
         public int getField1() {
@@ -78,7 +79,7 @@ public class TestCompositeIdRange {
         assertEquals(packedLong.getField2(), packedLong2.getField2());
     }
 
-    @Test(expected=IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testOutOfRange() {
         // Won't fit, we have allocated only 16 bits to field 1
         PackedLong packedLong = new PackedLong(Integer.MAX_VALUE, Integer.MAX_VALUE);
