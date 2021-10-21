4c60f3b8fd56afd00a0daa542e5a7189fa980fc6
Tim Veil <3260845+timveil@users.noreply.github.com>
2021-10-21 15:08:24 -0400

    beginning work on issue #47 (spanner support)

---
+++ src/main/resources/benchmarks/ycsb/ddl-spanner.sql
@@ -0,0 +1,14 @@
+drop table usertable;
+CREATE TABLE usertable (
+    ycsb_key INT64,
+    field1   STRING(MAX),
+    field2   STRING(MAX),
+    field3   STRING(MAX),
+    field4   STRING(MAX),
+    field5   STRING(MAX),
+    field6   STRING(MAX),
+    field7   STRING(MAX),
+    field8   STRING(MAX),
+    field9   STRING(MAX),
+    field10  STRING(MAX),
+) PRIMARY KEY (ycsb_key);
