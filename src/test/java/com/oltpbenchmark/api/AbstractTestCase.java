8e492f0aa6c5632d476a00508e6f14a641a3d4f8
Joseph Koshakow <koshy44@gmail.com>
2021-11-10 18:10:42 -0500

    Reimplement tests for StatementDialects
    The BenchmarkModule class creates a new instance of StatementDialects using
    the provided WorkloadConfiguration. StatementDialect then resolves the path to
    the dialect XML file using workloadConfiguration.getBenchmarkName(). The
    AbstractTestCase class was not setting the benchmark name within the
    WorkloadConfiguration. Therefore the existing StatementDialect tests failed to
    load in the XML files and would return before actually testing anything.
    
    This commit sets the benchmark name within WorkloadConfiguration for
    AbstractTestCases so they can properly load and test StatementDialects.
    
    Additionally this commit ports all of the old StatementDialect tests from
    TestStatementDialects to AbstractTestBenchmarkModule using the new API.
    
    Closes #25

--- src/test/java/com/oltpbenchmark/api/AbstractTestCase.java
+++ src/test/java/com/oltpbenchmark/api/AbstractTestCase.java
@@ -86,6 +86,7 @@ public abstract class AbstractTestCase<T extends BenchmarkModule> extends TestCa
         this.workConf.setDatabaseType(DB_TYPE);
         this.workConf.setUrl(DB_CONNECTION + this.dbName);
         this.workConf.setScaleFactor(DB_SCALE_FACTOR);
+        this.workConf.setBenchmarkName(BenchmarkModule.convertBenchmarkClassToBenchmarkName(clazz));
 
         this.benchmark = ClassUtil.newInstance(clazz,
                 new Object[]{this.workConf},
