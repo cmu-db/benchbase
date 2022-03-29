4f8411bee6103a323df50a45b1f2eb77d78c8a0d
Tim Veil <3260845+timveil@users.noreply.github.com>
2022-03-29 12:49:22 -0400

    adding back mockbenchmark

---
+++ src/test/java/com/oltpbenchmark/api/MockBenchmark.java
@@ -0,0 +1,44 @@
+/******************************************************************************
+ *  Copyright 2015 by OLTPBenchmark Project                                   *
+ *                                                                            *
+ *  Licensed under the Apache License, Version 2.0 (the "License");           *
+ *  you may not use this file except in compliance with the License.          *
+ *  You may obtain a copy of the License at                                   *
+ *                                                                            *
+ *    http://www.apache.org/licenses/LICENSE-2.0                              *
+ *                                                                            *
+ *  Unless required by applicable law or agreed to in writing, software       *
+ *  distributed under the License is distributed on an "AS IS" BASIS,         *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ *  See the License for the specific language governing permissions and       *
+ *  limitations under the License.                                            *
+ ******************************************************************************/
+
+package com.oltpbenchmark.api;
+
+import com.oltpbenchmark.WorkloadConfiguration;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MockBenchmark extends BenchmarkModule {
+    public MockBenchmark() {
+        super(new WorkloadConfiguration());
+        this.workConf.setBenchmarkName("mockbenchmark");
+    }
+
+    @Override
+    protected Package getProcedurePackageImpl() {
+        return null;
+    }
+
+    @Override
+    protected Loader<MockBenchmark> makeLoaderImpl() {
+        return null;
+    }
+
+    @Override
+    protected List<Worker<? extends BenchmarkModule>> makeWorkersImpl() throws IOException {
+        return null;
+    }
+}
