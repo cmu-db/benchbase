ff740530cc1c78524e982fba3cec01aeb64cbfb3
Brian Kroth <bpkroth@microsoft.com>
2022-04-22 14:56:41 -0500

    fixups to allow setting the UID/GID of the container user to match github actions runner uid/gid

--- docker/benchbase/run-dev-image.sh
+++ docker/benchbase/run-dev-image.sh
@@ -8,13 +8,5 @@ rootdir=$(readlink -f "$scriptdir/../../")
 cd "$scriptdir"
 ./build-dev-image.sh
 
-# Sync with Dockerfile
-containeruser_uid=1000
-
-if [ $EUID != $containeruser_uid ]; then
-    echo "NOTE: Local host user uid ($EUID) does not match container user uid $containeruser_uid." >&2
-    # TODO: Fix it up for them?
-fi
-
 cd "$rootdir"
 docker run -it --rm -v "$PWD:/benchbase" benchbase-dev
--- docker/benchbase/run-full-image.sh
+++ docker/benchbase/run-full-image.sh
@@ -10,14 +10,6 @@ rootdir=$(readlink -f "$scriptdir/../../")
 cd "$scriptdir"
 ./build-full-image.sh
 
-# Sync with Dockerfile
-containeruser_uid=1000
-
-if [ $EUID != $containeruser_uid ]; then
-    echo "NOTE: Local host user uid ($EUID) does not match container user uid $containeruser_uid." >&2
-    # TODO: Fix it up for them?
-fi
-
 cd "$rootdir"
 mkdir -p results/
 docker run -it --rm -v "$PWD/results:/benchbase/results" --env "BENCHBASE_PROFILE=$BENCHBASE_PROFILE" benchbase $*
