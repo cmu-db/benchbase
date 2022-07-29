92cfa5b2f187834bebc48f6895869c5d4b1e51eb
Brian Kroth <bpkroth@microsoft.com>
2022-07-29 10:25:36 -0500

    Revert "debugging"
    This reverts commit ae284dbc1badbc60bd8794c13be7198431475f5d.

--- docker/benchbase/fullimage/entrypoint.sh
+++ docker/benchbase/fullimage/entrypoint.sh
@@ -6,13 +6,10 @@ set -eu
 
 BENCHBASE_PROFILE="${BENCHBASE_PROFILE:-postgres}"
 cd /benchbase
-id || true
-ls -ld results || true
 echo "INFO: Using environment variable BENCHBASE_PROFILE=${BENCHBASE_PROFILE} with args: $*" >&2
 if ! [ -f "profiles/${BENCHBASE_PROFILE}/benchbase.jar" ]; then
     echo "ERROR: Couldn't find profile '${BENCHBASE_PROFILE}' in container image." >&2
     exit 1
 fi
 cd ./profiles/${BENCHBASE_PROFILE}/
-ls -ld results || true
 java -jar benchbase.jar $*
