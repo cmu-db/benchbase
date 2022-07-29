b832f777630a41e12c20da2e674418bfc2d1d359
Brian Kroth <bpkroth@microsoft.com>
2022-07-29 10:46:02 -0500

    check for the results directory

--- docker/benchbase/fullimage/entrypoint.sh
+++ docker/benchbase/fullimage/entrypoint.sh
@@ -12,4 +12,7 @@ if ! [ -f "profiles/${BENCHBASE_PROFILE}/benchbase.jar" ]; then
     exit 1
 fi
 cd ./profiles/${BENCHBASE_PROFILE}/
+if ! [ -d results/ ] || ! [ -w results/ ]; then
+    echo "ERROR: The results directory either doesn't exist or isn't writable." >&2
+fi
 java -jar benchbase.jar $*
