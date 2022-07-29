ed250e007278f64b75a133a7a9bdf621dd1d1127
Brian Kroth <bpkroth@microsoft.com>
2022-07-29 15:11:45 -0500

    no need for rootdir anymore

--- docker/benchbase/build-dev-image.sh
+++ docker/benchbase/build-dev-image.sh
@@ -5,8 +5,6 @@ set -eu
 scriptdir=$(dirname "$(readlink -f "$0")")
 cd "$scriptdir"
 . ./common-env.sh
-rootdir=$(readlink -f "$scriptdir/../../")
-cd "$rootdir"
 
 set -x
 # See comments in devcontainer/Dockerfile explaining /dev/null build context.
@@ -15,5 +13,5 @@ docker build --progress=plain \
     --build-arg MAVEN_OPTS="-Dhttp.proxyHost=${http_proxy_host} -Dhttp.proxyPort=${http_proxy_port} -Dhttps.proxyHost=${https_proxy_host} -Dhttps.proxyPort=${https_proxy_port}" \
     --build-arg BENCHBASE_PROFILES="${BENCHBASE_PROFILES}" \
     --build-arg CONTAINERUSER_UID="$CONTAINERUSER_UID" --build-arg CONTAINERUSER_GID="$CONTAINERUSER_GID" \
-    --tag benchbase-dev:latest ${image_tag_args:-} -f ./docker/benchbase/devcontainer/Dockerfile /dev/null
+    --tag benchbase-dev:latest ${image_tag_args:-} -f ./devcontainer/Dockerfile /dev/null
 set +x
