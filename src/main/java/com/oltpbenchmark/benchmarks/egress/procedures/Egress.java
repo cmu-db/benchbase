65b235dc8ce9a3d3981fa8d4833986d41feea3a5
Matt Butrovich <mbutrovich@gmail.com>
2022-08-12 13:42:31 -0400

    Missed some files in the last commit.

---
+++ src/main/java/com/oltpbenchmark/benchmarks/egress/procedures/Egress.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2020 by OLTPBenchmark Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.oltpbenchmark.benchmarks.egress.procedures;
+
+import com.oltpbenchmark.api.Procedure;
+import com.oltpbenchmark.api.SQLStmt;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * The actual Egress implementation
+ *
+ * @author mbutrovich
+ */
+public class Egress extends Procedure {
+    private static final Logger LOG = LoggerFactory.getLogger(Egress.class);
+
+
+    // There's no generic query for this benchmark so we leave it as an empty statement. Implement a DBMS-specific DDL
+    // and dialect as needed.
+    public final SQLStmt egressStmt = new SQLStmt(";");
+
+    public void run(Connection conn) {
+        try (PreparedStatement stmt = this.getPreparedStatement(conn, egressStmt)) {
+            stmt.executeQuery();
+            // We don't care about the ResultSet.
+        } catch (Exception ex) {
+            throw new RuntimeException("Egress statement failed.");
+        }
+    }
+
+}
