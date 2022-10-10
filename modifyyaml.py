23335883b88374a773529ed2c1c75e67d34439c0
Rakshit Jain <rjain@yugabyte.com>
2022-10-10 12:05:03 +0530

    fix modifyyaml

--- modifyyaml.py
+++ modifyyaml.py
@@ -4,18 +4,18 @@ import json
 import sys
 import yaml
 
+
 def main():
-    data=json.loads(sys.argv[1])
+    data = json.loads(sys.argv[1])
     with open(sys.argv[2]) as f:
+        doc = yaml.load(f, Loader=yaml.FullLoader)
+        for key, value in data.items():
+            doc[key] = value
+            print(key, value)
 
-        doc = yaml.load(f,Loader=yaml.FullLoader)
-        for key,value in data.items():
-            doc[key]=value
-            print(key,value)
-    
         with open(sys.argv[2], 'w') as fnew:
-            yaml.dump(doc, fnew)
-    return;
+            yaml.safe_dump(data, fnew, encoding='utf-8', allow_unicode=True)
+    return
 
 
-main(); 
+main();
