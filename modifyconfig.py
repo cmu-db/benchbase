57db85371531f601a45173c4b49f1ac2e566ff2e
Rakshit Jain <54314936+rakshitjain13@users.noreply.github.com>
2022-10-19 10:09:12 +0530

    fixed modify files + added load balance parameter to yb xml (#10)
    * fixed modify files + added load balance paramter to yb xmls
    
    * ssl fix
    
    * time fix
    
    * ssl mode fix
    
    * revert back tpcc condif
    
    * bool fix

--- modifyconfig.py
+++ modifyconfig.py
@@ -4,16 +4,32 @@ import sys
 import json
 
 
-tree = ET.parse('config/yugabyte/sample_{}_config.xml'.format(sys.argv[2]))
+tree = ET.parse('config/{}/sample_{}_config.xml'.format(sys.argv[3],sys.argv[2]))
 root = tree.getroot()
 
 data=json.loads(sys.argv[1])
 for key,value in data.items():
     element=root.find(key)
-    if element!=None :
-     element.text=value
-    print(key,value)
+    if key == "warmup":
+        element=root.find("works/work/warmup")
+        if element == None:
+            element = root.find("works/work")
+            newarmp = ET.SubElement(element,"warmup")
+            newarmp.text = value
+        else :
+            element.text = value
+    elif key == "time":
+        element=root.find("works/work/time")
+        if element != None:
+            element.text=value
+    elif element != None :
+        if key == "url":
+            element.text=element.text.replace("localhost",value)
+            if "sslmode" in data:
+              element.text = element.text.replace("disable","require") if data["sslmode"] else element.text.replace("require","disable")
+        else:
+            element.text=value
+        
 
-
-tree.write('config/yugabyte/sample_{}_config.xml'.format(sys.argv[2]))
+tree.write('config/{}/sample_{}_config.xml'.format(sys.argv[3],sys.argv[2]))
 # python modifyconfig.py '{"url":"jdbc:postgresql://localhost:5433/yugabyte","username":"yugabyte","password":""}' seats
--- modifyyaml.py
+++ modifyyaml.py
@@ -10,7 +10,14 @@ def main():
     with open(sys.argv[2]) as f:
         doc = yaml.load(f, Loader=yaml.FullLoader)
         for key, value in data.items():
-            doc[key] = value
+            if key=="warmup":
+                doc["works"]["work"]["warmup"] = value
+            elif key == "url":
+                doc[key] = doc[key].replace("localhost",value)
+                if "sslmode" in data:
+                 doc[key] = doc[key].replace("disable","require") if data["sslmode"] else doc[key].replace("require","disable")
+            else:
+                doc[key] = value
             print(key, value)
 
         with open(sys.argv[2], 'w') as fnew:
