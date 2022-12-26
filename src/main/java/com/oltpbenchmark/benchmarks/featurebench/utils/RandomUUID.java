e63971f54263c11ba1bff056859d2725a3519f3a
Hemant Bhanawat <hemant9379@gmail.com>
2022-12-26 12:34:09 +0530

    UUID fixes (#49)
    UUID needs to be passed as a UUID object rather than a string.

--- src/main/java/com/oltpbenchmark/benchmarks/featurebench/utils/RandomUUID.java
+++ src/main/java/com/oltpbenchmark/benchmarks/featurebench/utils/RandomUUID.java
@@ -3,7 +3,6 @@ package com.oltpbenchmark.benchmarks.featurebench.utils;
 import java.util.List;
 import java.util.UUID;
 
-
 public class RandomUUID implements BaseUtil {
 
     public RandomUUID(List<Object> values) {
@@ -14,17 +13,12 @@ public class RandomUUID implements BaseUtil {
     }
 
     public RandomUUID(List<Object> values, int workerId, int totalWorkers) {
-        if (values.size() != 0) {
-            throw new RuntimeException("Incorrect number of parameters for util function "
-                + this.getClass());
-        }
+        this(values);
     }
 
     @Override
     public Object run() {
-        UUID uuid = UUID.randomUUID();
-        String uuidAsString = uuid.toString();
-        return uuidAsString;
+        return UUID.randomUUID();
     }
 }
 
