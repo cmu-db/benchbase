b0156b4cb3cfb427eee324f7e7ae7779d5e2a8a7
Brian Kroth <bpkroth@microsoft.com>
2023-03-31 20:16:46 +0000

    fixups for build in container script

--- docker/benchbase/devcontainer/build-in-container.sh
+++ docker/benchbase/devcontainer/build-in-container.sh
@@ -30,7 +30,7 @@ function build_profile() {
     mvn -T 2C -B --file pom.xml package -D descriptors=src/main/assembly/dir.xml -P $profile \
         $SKIP_TEST_ARGS $EXTRA_MAVEN_ARGS -D buildDirectory=target/$profile
     # Copy the resultant output to the profiles directory.
-    cp -rulv target/benchbase-$profile/benchbase-$profile profiles/$profile
+    cp -rlv target/$profile/benchbase-$profile/benchbase-$profile profiles/$profile
     # Later the container entrypoint will move into this directory to run it, so
     # save all of the results back to the common volume mapping location.
     ln -s ../../results profiles/$profile/results
