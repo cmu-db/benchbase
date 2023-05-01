3191a9cec730513fb754e58612e4a48c5389eab6
anjagruenheid <87397397+anjagruenheid@users.noreply.github.com>
2023-05-01 18:51:30 +0200

    Change formatting of TPC-C queries. (#305)
    Co-authored-by: Brian Kroth <bpkroth@users.noreply.github.com>

--- src/main/java/com/oltpbenchmark/benchmarks/tpcc/procedures/StockLevel.java
+++ src/main/java/com/oltpbenchmark/benchmarks/tpcc/procedures/StockLevel.java
@@ -35,21 +35,25 @@ public class StockLevel extends TPCCProcedure {
     private static final Logger LOG = LoggerFactory.getLogger(StockLevel.class);
 
     public SQLStmt stockGetDistOrderIdSQL = new SQLStmt(
-            "SELECT D_NEXT_O_ID " +
-            "  FROM " + TPCCConstants.TABLENAME_DISTRICT +
-            " WHERE D_W_ID = ? " +
-            "   AND D_ID = ?");
+    """
+        SELECT D_NEXT_O_ID 
+          FROM  %s
+         WHERE D_W_ID = ? 
+           AND D_ID = ?
+    """.formatted(TPCCConstants.TABLENAME_DISTRICT));
 
     public SQLStmt stockGetCountStockSQL = new SQLStmt(
-            "SELECT COUNT(DISTINCT (S_I_ID)) AS STOCK_COUNT " +
-            " FROM " + TPCCConstants.TABLENAME_ORDERLINE + ", " + TPCCConstants.TABLENAME_STOCK +
-            " WHERE OL_W_ID = ?" +
-            " AND OL_D_ID = ?" +
-            " AND OL_O_ID < ?" +
-            " AND OL_O_ID >= ?" +
-            " AND S_W_ID = ?" +
-            " AND S_I_ID = OL_I_ID" +
-            " AND S_QUANTITY < ?");
+    """
+        SELECT COUNT(DISTINCT (S_I_ID)) AS STOCK_COUNT 
+         FROM  %s, %s
+         WHERE OL_W_ID = ?
+         AND OL_D_ID = ?
+         AND OL_O_ID < ?
+         AND OL_O_ID >= ?
+         AND S_W_ID = ?
+         AND S_I_ID = OL_I_ID
+         AND S_QUANTITY < ?
+    """.formatted(TPCCConstants.TABLENAME_ORDERLINE, TPCCConstants.TABLENAME_STOCK));
 
     public void run(Connection conn, Random gen, int w_id, int numWarehouses, int terminalDistrictLowerID, int terminalDistrictUpperID, TPCCWorker w) throws SQLException {
 
