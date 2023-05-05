a7943fb7e7a24b1c2937a91149738c340e5f2d6f
Matt Butrovich <mbutrovich@users.noreply.github.com>
2023-05-05 09:59:03 -0400

    Accidentally removed index from NoisePage.

--- src/main/resources/benchmarks/tpcc/ddl-noisepage.sql
+++ src/main/resources/benchmarks/tpcc/ddl-noisepage.sql
@@ -145,3 +145,5 @@ CREATE TABLE order_line (
     FOREIGN KEY (ol_supply_w_id, ol_i_id) REFERENCES stock (s_w_id, s_i_id) ON DELETE CASCADE, */
     PRIMARY KEY (ol_w_id, ol_d_id, ol_o_id, ol_number)
 );
+
+CREATE INDEX idx_customer_name ON customer (c_w_id, c_d_id, c_last);
