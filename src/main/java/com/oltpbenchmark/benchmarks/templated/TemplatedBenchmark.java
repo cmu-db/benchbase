041ef094fe7c2faa30c2763f3d60b195f26c46c6
Anja Gruenheid <agruenheid@microsoft.com>
2023-09-26 09:39:13 +0200

    Error instead of warning.

--- src/main/java/com/oltpbenchmark/benchmarks/templated/TemplatedBenchmark.java
+++ src/main/java/com/oltpbenchmark/benchmarks/templated/TemplatedBenchmark.java
@@ -97,7 +97,7 @@ public class TemplatedBenchmark extends BenchmarkModule {
             for (Entry<TransactionType, Procedure> kv : getProcedures().entrySet()) {
                 // Sanity check that the procedure has the right type.
                 if (!(kv.getValue() instanceof GenericQuery)) {
-                    LOG.warn(
+                    LOG.error(
                         String.format(
                             "Procedure %s does not have the correct class type (GenericQuery).",
                         kv.getValue().toString()));
