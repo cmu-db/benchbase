d1c6e4754f36eb5dc9f3cff391e3fc06a5e3b7b6
Nguyen Nguyen <nguyen.b.nguyen@oracle.com>
2023-10-18 12:35:14 +1000

    Move to Oracle 21 XE

--- docker/oracle/reset.sql
+++ docker/oracle/reset.sql
@@ -1,8 +1,8 @@
 -- Copyright (c) 2023,  Oracle and/or its affiliates.
 -- Non-DBA user specified in sample config file
-alter session set container = FREEPDB1;
-create user benchbase identified by password;
-GRANT CONNECT, RESOURCE, UNLIMITED TABLESPACE TO benchbase;
+DROP USER benchbase CASCADE;
+CREATE USER benchbase IDENTIFIED BY password;
+GRANT CONNECT, RESOURCE, CREATE VIEW, UNLIMITED TABLESPACE TO benchbase;
 -- Resourcestresser benchmark requires these two packages from SYS
 GRANT EXECUTE ON DBMS_CRYPTO TO benchbase;
 GRANT EXECUTE ON DBMS_LOCK TO benchbase;
