5220c3f857c9d01289bdf39de332f3b86a0cb239
Brian Kroth <bpkroth@microsoft.com>
2023-12-08 18:23:17 +0000

    convert throw type to avoid InterruptedException handling warnings

--- src/main/java/com/oltpbenchmark/util/TableDataIterable.java
+++ src/main/java/com/oltpbenchmark/util/TableDataIterable.java
@@ -24,6 +24,7 @@ import com.opencsv.CSVReader;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.Iterator;
 
 /**
@@ -80,7 +81,7 @@ public class TableDataIterable implements Iterable<Object[]>, AutoCloseable {
     }
 
     @Override
-    public void close() throws Exception {
+    public void close() throws IOException {
         if (this.in != null) {
             in.close();
         }
