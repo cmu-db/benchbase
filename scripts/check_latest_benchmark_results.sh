52a8a040e112dc0e425ca6e5344c2892d220bb38
Brian Kroth <bpkroth@microsoft.com>
2023-12-14 18:29:50 +0000

    fixup script

--- scripts/check_latest_benchmark_results.sh
+++ scripts/check_latest_benchmark_results.sh
@@ -41,18 +41,39 @@ if ! type xmllint 2>/dev/null; then
     exit 1
 fi
 
-# FIXME: Doesn't currently handle multiple workloads.
-expected_runtime=$(xmllint --xpath '//works/work/time/text()' "$config_file" || true)
 # TODO: include warmup?
+expected_runtime=$(xmllint --xpath '//works/work/time/text()' "$config_file" | awk '{ print sum=sum+$1 }' | tail -n1)
 
-if [ -z "$expected_runtime" ]; then
-    # FIXME: Doesn't currently handle serial benchmarks.
-    echo "ERROR: Failed to find expected runtime in config file: $config_file" >&2
-    exit 1
-fi
+if xmllint --xpath '//works/work/serial/text()' "$config_file" | grep -q -x true; then
+    if [ -n "$expected_runtime" ]; then
+        echo "ERROR: Unhandled: Found expected runtime in config file for serial workloads." >&2
+        exit 1
+    fi
 
-elapsed_time=$(cat "$summary_json" | jq -e '(.["Elapsed Time (nanoseconds)"] / 1000000000 | round)')
-if [ "$elapsed_time" -lt "$expected_runtime" ]; then
-    echo "ERROR: Benchmark elapsed runtime ($elapsed_time) was less than expected ($expected_runtime) or failed to parse output." >&2
-    exit 1
+    expected_query_count=$(xmllint --xpath '//works/work/weights/text()' "$config_file" | sed 's/,/\n/g' | grep -c -x 1 || true)
+    if xmllint --xpath '//works/work/weights/text()' "$config_file" | sed 's/,/\n/g' | grep -q -v '^[01]$'; then
+        echo "ERROR: Unsupported weight specification for serial workloads.  Only 0,1 are handled currently." >&2
+        exit 1
+    fi
+
+    measured_requests=$(cat "$summary_json" | jq -e '.["Measured Requests"]')
+    if [ "$measured_requests" -ne "$expected_query_count" ]; then
+        echo "ERROR: Benchmark measured requests ($measured_requests) was less than expected ($expected_query_count) or failed to parse output." >&2
+        exit 1
+    else
+        echo "OK: Benchmark measured requests ($measured_requests) matched ($expected_query_count)."
+    fi
+else
+    if [ -z "$expected_runtime" ]; then
+        echo "ERROR: Failed to find expected runtime in config file: $config_file" >&2
+        exit 1
+    fi
+
+    elapsed_time=$(cat "$summary_json" | jq -e '.["Elapsed Time (nanoseconds)"] / 1000000000 | round')
+    if [ "$elapsed_time" -lt "$expected_runtime" ]; then
+        echo "ERROR: Benchmark elapsed runtime ($elapsed_time) was less than expected ($expected_runtime) or failed to parse output." >&2
+        exit 1
+    else
+        echo "OK: Benchmark elapsed runtime ($elapsed_time) matched expected ($expected_runtime)."
+    fi
 fi
