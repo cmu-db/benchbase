9d1eee9b2c0a23ba8f6b729759d6c8d420f7f72c
Preethi1609 <preethi.n1609@gmail.com>
2024-04-29 21:27:20 -0400

    fix cockroachdb setup script

--- setup_scripts/cockroach_init.sh
+++ setup_scripts/cockroach_init.sh
@@ -1,7 +1,5 @@
 wget https://binaries.cockroachdb.com/cockroach-v24.1.0-alpha.5.linux-amd64.tgz
-tar -xvf cockroach-archive.tar.gz
-# tar -xvf cockroach-v24.1.0-alpha.5.linux-amd64.tgz
+tar -xvf cockroach-v24.1.0-alpha.5.linux-amd64.tgz
 cd cockroach-v24.1.0-alpha.5.linux-amd64
 sudo mv cockroach /usr/local/bin/
 sudo chmod +x /usr/local/bin/cockroach
-cockroach start-single-node --insecure
