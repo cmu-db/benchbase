b45368c901c1dcc880548567c597588306199ab1
Brian Kroth <bpkroth@microsoft.com>
2024-05-10 19:27:40 +0000

    Enable use of pg_stat_statement extension in local docker-compose and CI pipeline

--- docker/postgres-latest/up.sh
+++ docker/postgres-latest/up.sh
@@ -5,3 +5,21 @@ scriptdir=$(dirname "$(readlink -f "$0")")
 cd "$scriptdir/"
 
 docker compose up -d
+
+# Wait until ready
+for i in {1..30}; do
+    if docker exec postgres pg_isready; then
+        break
+    else
+        sleep 1
+    fi
+done
+
+function run_psql_in_docker() {
+    set -x
+    docker exec --env PGPASSWORD=password postgres psql -h localhost -U admin benchbase --csv -c "$@"
+    set +x
+}
+
+run_psql_in_docker "CREATE EXTENSION pg_stat_statements"
+run_psql_in_docker "SELECT COUNT(*) FROM pg_stat_statements" | egrep '^[1-9][0-9]*$'
