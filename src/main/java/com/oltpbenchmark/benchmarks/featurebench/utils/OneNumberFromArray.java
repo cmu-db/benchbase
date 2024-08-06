cdebf5b6179646ee573f37d0094b1792dee6b637
shaharuk-yb <sshaikh@yugabyte.com>
2024-08-06 12:58:49 +0530

    utility function OneNumberFromArray convert to Integer if possible

--- src/main/java/com/oltpbenchmark/benchmarks/featurebench/utils/OneNumberFromArray.java
+++ src/main/java/com/oltpbenchmark/benchmarks/featurebench/utils/OneNumberFromArray.java
@@ -15,17 +15,32 @@ public class OneNumberFromArray implements BaseUtil {
         }
         listOfIntegers = new ArrayList<>();
         for (Object value : values) {
-            listOfIntegers.add((Integer) value);
+            listOfIntegers.add(convertToInteger(value));
         }
     }
-    public OneNumberFromArray(List<Object> values,int workerId,int totalWorkers) {
+
+    public OneNumberFromArray(List<Object> values, int workerId, int totalWorkers) {
         if (values.size() == 0) {
             throw new RuntimeException("Incorrect number of parameters for util function "
                 + this.getClass());
         }
         listOfIntegers = new ArrayList<>();
         for (Object value : values) {
-            listOfIntegers.add((Integer) value);
+            listOfIntegers.add(convertToInteger(value));
+        }
+    }
+
+    private Integer convertToInteger(Object value) {
+        if (value instanceof Integer) {
+            return (Integer) value;
+        } else if (value instanceof String) {
+            try {
+                return Integer.parseInt((String) value);
+            } catch (NumberFormatException e) {
+                throw new RuntimeException("Cannot convert value to Integer: " + value, e);
+            }
+        } else {
+            throw new RuntimeException("Unsupported type for conversion to Integer: " + value.getClass());
         }
     }
 
