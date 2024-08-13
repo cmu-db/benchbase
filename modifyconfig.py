a2111315d8493ab515c4e269b90bd7f9713da859
chahat agrawal <42664727+chahatagrawal117@users.noreply.github.com>
2024-08-13 15:33:29 +0530

    Adding support for loaderthreads in config file (#139)
    * Adding support for loaderthreads in config file
    
    * fix
    
    ---------
    
    Co-authored-by: Chahat Agrawal <chahat@ca-mbp-31whw.tail5955.ts.net>

--- modifyconfig.py
+++ modifyconfig.py
@@ -2,35 +2,50 @@ from logging import root
 import xml.etree.ElementTree as ET
 import sys
 import json
+import re
+data = json.loads(sys.argv[1])
 
-
-tree = ET.parse('config/{}/sample_{}_config.xml'.format(sys.argv[3],sys.argv[2]))
+tree = ET.parse('config/{}/sample_{}_config.xml'.format(sys.argv[3], sys.argv[2]))
 root = tree.getroot()
 
-data=json.loads(sys.argv[1])
-for key,value in data.items():
-    element=root.find(key)
+
+
+for key, value in data.items():
+    element = root.find(key)
     value = str(value)
     if key == "warmup":
-        element=root.find("works/work/warmup")
+        element = root.find("works/work/warmup")
         if element == None:
             element = root.find("works/work")
-            newarmp = ET.SubElement(element,"warmup")
+            newarmp = ET.SubElement(element, "warmup")
             newarmp.text = value
-        else :
+        else:
             element.text = value
     elif key == "time":
-        element=root.find("works/work/time")
+        element = root.find("works/work/time")
         if element != None:
-            element.text=value
-    elif element != None :
-        if key == "url":
-            element.text=element.text.replace("localhost",value)
-            if "sslmode" in data:
-              element.text = element.text.replace("disable","require") if data["sslmode"] else element.text.replace("require","disable")
-        else:
-            element.text=value
-        
+            element.text = value
+    # Check if loaderThreads is in the data
+    elif key == 'loaderThreads':
+        loader_threads_value = str(data['loaderThreads'])
 
-tree.write('config/{}/sample_{}_config.xml'.format(sys.argv[3],sys.argv[2]))
+        # Check if <loaderThreads> already exists
+        element = root.find("loaderThreads")
+        if element is None:
+            # Create <loaderThreads> element at the top level (direct child of root)
+            new_loader_threads = ET.SubElement(root, "loaderThreads")
+            new_loader_threads.text = loader_threads_value
+        else:
+            # If it exists, update its value
+            element.text = loader_threads_value
+    elif element != None:
+        if key == "url":
+            element.text = element.text.replace("localhost", value)
+            if "sslmode" in data:
+                element.text = element.text.replace("disable", "require") if data["sslmode"] else element.text.replace(
+                    "require", "disable")
+        else:
+            element.text = value
+
+tree.write('config/{}/sample_{}_config.xml'.format(sys.argv[3], sys.argv[2]))
 # python modifyconfig.py '{"url":"jdbc:postgresql://localhost:5433/yugabyte","username":"yugabyte","password":""}' seats
