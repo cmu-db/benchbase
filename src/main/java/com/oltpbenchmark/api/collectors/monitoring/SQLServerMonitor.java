3591ccd89ecd954c721630a8bcc7ecf87fbfbded
Brian Kroth <bpkroth@gmail.com>
2025-02-14 16:31:32 +0000

    format

--- src/main/java/com/oltpbenchmark/api/collectors/monitoring/SQLServerMonitor.java
+++ src/main/java/com/oltpbenchmark/api/collectors/monitoring/SQLServerMonitor.java
@@ -21,8 +21,8 @@ import java.util.regex.Pattern;
 
 /**
  * Implementation of a monitor specific to SQLServer. Uses SQLServer's system tables to extract
- * relevant query and system information.
- * Note: Requires "VIEW SERVER PERFORMANCE STATE" permissions.
+ * relevant query and system information. Note: Requires "VIEW SERVER PERFORMANCE STATE"
+ * permissions.
  */
 public class SQLServerMonitor extends DatabaseMonitor {
 
